c4417ce5859508c9da7e5857a947cd286138921e
mike <mike@d14347b8-48fd-0310-9e84-97907d434d85>
2006-08-02 14:59:09 +0000

    Added some framework structure for the new Engine2D system.
    git-svn-id: svn://10.0.50.55/puffbomb/trunk@1212 d14347b8-48fd-0310-9e84-97907d434d85

---
+++ Code/Engine2D/Engine2D.h
@@ -0,0 +1,49 @@
+// - ------------------------------------------------------------------------------------------ - //
+// Engine2D //
+// - ------------------------------------------------------------------------------------------ - //
+#ifndef __Engine2D_Engine2D_H__
+#define __Engine2D_Engine2D_H__
+// - ------------------------------------------------------------------------------------------ - //
+#include <vector>
+
+#include <Engine/DynamicObject.h>
+#include <Engine/StaticObject.h>
+#include <Engine/PassiveObject.h>
+#include <Engine/Zone.h>
+
+#include <Physics/Impulse.h>
+// - ------------------------------------------------------------------------------------------ - //
+namespace Engine2D {
+// - ------------------------------------------------------------------------------------------ - //
+class cEngine2D {
+public:
+	// Current instance of the Engine. Step and Draw update this, so to support multiple engines. //
+	static cEngine2D* Current;
+public:
+	
+
+public:
+	// Our in game entities //
+	std::vector< cDynamicObject > DynamicObject;
+	std::vector< cStaticObject > StaticObject;
+	std::vector< cPassiveObject > PassiveObject;
+	std::vector< cZone > Zone;
+
+	// A collection of impulse forces to apply to dynamic objects //
+	std::vector< cImpulse > Impulse;
+
+
+public:
+	// Move and Display Everything //
+	void Step();
+	void Draw();
+
+	// Reset game back to a neutral state //
+	void Reset();
+	
+};
+// - ------------------------------------------------------------------------------------------ - //
+}; // namespace Engine2D //
+// - ------------------------------------------------------------------------------------------ - //
+#endif // __Engine2D_Engine2D_H__ //
+// - ------------------------------------------------------------------------------------------ - //
