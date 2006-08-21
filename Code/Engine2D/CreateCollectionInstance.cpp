7284b49b0615b91bfab5c1018d196e6b86715004
mike <mike@d14347b8-48fd-0310-9e84-97907d434d85>
2006-08-21 08:29:09 +0000

    Added Collection instance function and pose.
    git-svn-id: svn://10.0.50.55/puffbomb/trunk@1400 d14347b8-48fd-0310-9e84-97907d434d85

---
+++ Code/Engine2D/CreateCollectionInstance.cpp
@@ -0,0 +1,28 @@
+// - ------------------------------------------------------------------------------------------ - //
+// CreateCollectionInstance //
+// - ------------------------------------------------------------------------------------------ - //
+#include "CreateCollectionInstance.h"
+// - ------------------------------------------------------------------------------------------ - //
+namespace Engine2D {
+// - ------------------------------------------------------------------------------------------ - //
+cDynamicObjectCollection* CreateCollectionInstance( unsigned int Id, Vector2D Pos, int Argument ) {
+	// Switch based on the Id passed to the function //
+	switch ( Id ) {
+		case 1: {
+			//return new cDynamicObjectCollection( "Hamster.obj", Pos );
+			break;
+		}
+		
+		default: {
+			// No object with Id requested found, so return a null object //
+			return 0;
+			break;
+		}
+	}
+	
+	// In case the function accidentially doesn't return anything //
+	return 0;
+}
+// - ------------------------------------------------------------------------------------------ - //
+}; // namespace Engine2D //
+// - ------------------------------------------------------------------------------------------ - //
