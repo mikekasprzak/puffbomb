81859d7949be3faf38e257a68562b47f419520d3
rich <rich@d14347b8-48fd-0310-9e84-97907d434d85>
2006-09-01 10:47:15 +0000

    Zooming and panning is now working in the collection editor.
    git-svn-id: svn://10.0.50.55/puffbomb/trunk@1461 d14347b8-48fd-0310-9e84-97907d434d85

--- Code/Editor/CollectionEditor/CollectionEdit.cpp
+++ Code/Editor/CollectionEditor/CollectionEdit.cpp
@@ -42,7 +42,33 @@ void cCollectionEdit::HudDraw()
 // - ------------------------------------------------------------------------------------------ - //
 void cCollectionEdit::Step()
 {
+	CurMousePos = CalcMousePos();
+
+	if( Button[ MOUSE_1 ].Pressed() )
+	{
+		OldMousePos = CurMousePos;
+	}
+
+	// Handles scrolling around the map
+	Scroll( Camera );
 	
+	// Handles the zooming in and out of a map
+	Zoom( Real( 64.0 ), Camera );
+}
+// - ------------------------------------------------------------------------------------------ - //
+Vector2D cCollectionEdit::CalcMousePos()
+{
+	Vector2D tempMousPos = Vector2D(
+			Real( ( int( Mouse.x * Real( cGlobal::HudW ) ) )
+			- ( -Camera->Pos.x / Real( Camera->Pos.z / cGlobal::HudZoom ) )
+			- ( Real(cGlobal::HudW >> 1) ) )
+			* Real( Camera->Pos.z / cGlobal::HudZoom ),
+			Real( ( int( -Mouse.y * Real( cGlobal::HudH ) ) )
+			+ ( Camera->Pos.y / Real( Camera->Pos.z / cGlobal::HudZoom ) )
+			+ ( cGlobal::HudH >> 1 ) )
+			* Real( Camera->Pos.z / cGlobal::HudZoom )
+	);
+	return tempMousPos;
 }
 // - ------------------------------------------------------------------------------------------ - //
 #endif // Editor //
