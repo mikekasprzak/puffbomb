8205d1e921a10ae2120bf469c846d48da2e4e679
rich <rich@d14347b8-48fd-0310-9e84-97907d434d85>
2006-12-07 00:25:22 +0000

    fixed the near clip for the splashscreen and main menu as well.
    git-svn-id: svn://10.0.50.55/puffbomb/trunk@1876 d14347b8-48fd-0310-9e84-97907d434d85

--- Code/Framework/SplashScreen/SplashScreen.cpp
+++ Code/Framework/SplashScreen/SplashScreen.cpp
@@ -17,7 +17,7 @@ cSplashScreen::cSplashScreen() :
 		Vector3D( 0.0, 1.0, 0.0 ),						// Up
 		45.0,											// Field of View
 		Global::AspectRatio,							// Aspect Ratio
-		1.0,											// NearClip
+		1000.0,											// NearClip
 		100000.0,										// FarClip
 		Global::HudZoom,								// MinZoom
 		Global::HudZoom,								// MaxZoom
