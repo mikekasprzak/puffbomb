fb836df333df356c36148e5e08f28719038c03d5
rich <rich@d14347b8-48fd-0310-9e84-97907d434d85>
2006-12-31 21:03:16 +0000

    added more classic levels.
    git-svn-id: svn://10.0.50.55/puffbomb/trunk@2097 d14347b8-48fd-0310-9e84-97907d434d85

--- Code/Game/PuffBOMB/SuperFlow.cpp
+++ Code/Game/PuffBOMB/SuperFlow.cpp
@@ -58,7 +58,7 @@ cSuperFlow::cSuperFlow() :
 	ClassicPath( "Maps/Classic/" ),
 	GolfPath( "Maps/Golf/" ),
 	Players( 4 ),
-	CurClassicMap( 2 ),
+	CurClassicMap( 1 ),
 	CurGolfMap( 0 )
 {
 	SetHudData();
@@ -108,7 +108,7 @@ void cSuperFlow::StateFlow()
 			case stMainMenu:
 			case stBackMainMenu:
 			{
-				CurClassicMap = 2;
+				CurClassicMap = 1;
 				CurGolfMap = 0;
 				
 				// Display the MainMenu screen //
