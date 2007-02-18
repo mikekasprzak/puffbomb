6f630f9a9e0066a37698133e03700bc3f065c596
mike <mike@d14347b8-48fd-0310-9e84-97907d434d85>
2007-02-18 13:19:45 +0000

    Added proper bounds checking asserts to GetPose in the Component Animator.
    git-svn-id: svn://10.0.50.55/puffbomb/trunk@2329 d14347b8-48fd-0310-9e84-97907d434d85

--- Code/Engine2D/DynamicComponent/DynamicComponentAnimator.cpp
+++ Code/Engine2D/DynamicComponent/DynamicComponentAnimator.cpp
@@ -20,16 +20,52 @@ cDynamicComponentAnimator::cDynamicComponentAnimator( cComponentAnimationSet* _A
 
 // - ------------------------------------------------------------------------------------------ - //
 cBody2DPose* cDynamicComponentAnimator::GetPose() const {
-	// Todo: Assert bounds check CurrentAnimation and CurrentFrame //
-//	Assert( ,
-//		"CurrentFrame out of bounds!" );
+	// Current Animation Bounds Checking //
+	Assert( CurrentAnimation < 0,
+		"CurrentAnimation out of bounds!  " << CurrentAnimation << " of " <<
+		AnimationSet->Animation.size()
+		);
+	Assert( CurrentAnimation >= (int)AnimationSet->Animation.size(),
+		"CurrentAnimation out of bounds!  " << CurrentAnimation << " of " <<
+		AnimationSet->Animation.size()
+		);
+
+	// Current Frame Bounds Checking //
+	Assert( CurrentFrame < 0,
+		"CurrentFrame out of bounds!  " << CurrentFrame << " of " <<
+		AnimationSet->Animation[ CurrentAnimation ].Frame.size()
+		);
+	Assert( CurrentFrame >= (int)AnimationSet->Animation[ CurrentAnimation ].Frame.size(),
+		"CurrentFrame out of bounds!  " << CurrentFrame << " of " <<
+		AnimationSet->Animation[ CurrentAnimation ].Frame.size()
+		);
 	
+	// Return the requested pose //
 	return &AnimationSet->BodyPose[ AnimationSet->Animation[ CurrentAnimation ].Frame[ CurrentFrame ].BodyPoseIndex ];
 }
 // - ------------------------------------------------------------------------------------------ - //
 cBody2DPose* cDynamicComponentAnimator::GetPose( const int _Animation, const int _Frame ) const {
-	// Todo: Assert bounds check _Animation and _Frame //
+	// _Animation Bounds Checking //
+	Assert( _Animation < 0,
+		"_Animation out of bounds!  " << _Animation << " of " <<
+		AnimationSet->Animation.size()
+		);
+	Assert( _Animation >= (int)AnimationSet->Animation.size(),
+		"_Animation out of bounds!  " << _Animation << " of " <<
+		AnimationSet->Animation.size()
+		);
 
+	// _Frame Bounds Checking //
+	Assert( _Frame < 0,
+		"_Frame out of bounds!  " << _Frame << " of " <<
+		AnimationSet->Animation[ _Animation ].Frame.size()
+		);
+	Assert( _Frame >= (int)AnimationSet->Animation[ _Animation ].Frame.size(),
+		"_Frame out of bounds!  " << _Frame << " of " <<
+		AnimationSet->Animation[ _Animation ].Frame.size()
+		);
+
+	// Return the requested pose //
 	return &AnimationSet->BodyPose[ AnimationSet->Animation[ _Animation ].Frame[ _Frame ].BodyPoseIndex ];
 }
 // - ------------------------------------------------------------------------------------------ - //
