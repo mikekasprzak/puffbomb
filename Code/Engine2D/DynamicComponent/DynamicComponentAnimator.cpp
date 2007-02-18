a17d0767898967592164abe5383d0c6385609a91
mike <mike@d14347b8-48fd-0310-9e84-97907d434d85>
2007-02-18 11:41:34 +0000

    DynamicComponentAnimator now uses the basic Animator.
    git-svn-id: svn://10.0.50.55/puffbomb/trunk@2325 d14347b8-48fd-0310-9e84-97907d434d85

--- Code/Engine2D/DynamicComponent/DynamicComponentAnimator.cpp
+++ Code/Engine2D/DynamicComponent/DynamicComponentAnimator.cpp
@@ -9,47 +9,49 @@
 namespace Engine2D {
 // - ------------------------------------------------------------------------------------------ - //
 cDynamicComponentAnimator::cDynamicComponentAnimator( ) :
-	AnimationSet( 0 )
+	//AnimationSet( 0 )
+	cBasicAnimator< cComponentAnimationSet >()
 {
 }
 // - ------------------------------------------------------------------------------------------ - //
 cDynamicComponentAnimator::cDynamicComponentAnimator( cComponentAnimationSet* _AnimationSet ) :
-	AnimationSet( _AnimationSet ),
-	CurrentAnimation( 0 ),
-	CurrentFrame( 0 ),
-	CurrentFrameTime( Real::Zero ),
-	PlayBackRate( Real::One )
+	cBasicAnimator< cComponentAnimationSet >( _AnimationSet )
+//	AnimationSet( _AnimationSet ),
+//	CurrentAnimation( 0 ),
+//	CurrentFrame( 0 ),
+//	CurrentFrameTime( Real::Zero ),
+//	PlayBackRate( Real::One )
 {
 }
 // - ------------------------------------------------------------------------------------------ - //
-void cDynamicComponentAnimator::Step() {
-	// Make sure we actually have an associated animation set //
-	if ( AnimationSet ) {
-		// Step our current frame time forward by our rate of playback //
-		CurrentFrameTime += PlayBackRate;
-		
-		// If our current frame time breaks the hold length of the current frame //
-		if ( CurrentFrameTime >= Real( AnimationSet->Animation[ CurrentAnimation ].Frame[ CurrentFrame ].Time ) ) {
-			// Step to the next frame //
-			CurrentFrame++;
-			
-			// If our frame hits the end //
-			if ( CurrentFrame >= (int)AnimationSet->Animation[ CurrentAnimation ].Frame.size() ) {
-				// Set frame to the loop point //
-				CurrentFrame = AnimationSet->Animation[ CurrentAnimation ].LoopPoint;
-				
-				// Set looped flag //
-				AnimationFlags.SetLooped();
-			}
-
-			// Update the Pose //
-//			Body.Pose = &AnimationSet->BodyPose[ AnimationSet->Animation[ CurrentAnimation ].Frame[ CurrentFrame ].BodyPoseIndex ];
-			
-			// Offset the Current Frame Time by the hold of the previous, to correctly accumulate //
-			CurrentFrameTime -= Real( AnimationSet->Animation[ CurrentAnimation ].Frame[ CurrentFrame ].Time );
-		}
-	}	
-}
+//void cDynamicComponentAnimator::Step() {
+//	// Make sure we actually have an associated animation set //
+//	if ( AnimationSet ) {
+//		// Step our current frame time forward by our rate of playback //
+//		CurrentFrameTime += PlayBackRate;
+//		
+//		// If our current frame time breaks the hold length of the current frame //
+//		if ( CurrentFrameTime >= Real( AnimationSet->Animation[ CurrentAnimation ].Frame[ CurrentFrame ].Time ) ) {
+//			// Step to the next frame //
+//			CurrentFrame++;
+//			
+//			// If our frame hits the end //
+//			if ( CurrentFrame >= (int)AnimationSet->Animation[ CurrentAnimation ].Frame.size() ) {
+//				// Set frame to the loop point //
+//				CurrentFrame = AnimationSet->Animation[ CurrentAnimation ].LoopPoint;
+//				
+//				// Set looped flag //
+//				AnimationFlags.SetLooped();
+//			}
+//
+//			// Update the Pose //
+////			Body.Pose = &AnimationSet->BodyPose[ AnimationSet->Animation[ CurrentAnimation ].Frame[ CurrentFrame ].BodyPoseIndex ];
+//			
+//			// Offset the Current Frame Time by the hold of the previous, to correctly accumulate //
+//			CurrentFrameTime -= Real( AnimationSet->Animation[ CurrentAnimation ].Frame[ CurrentFrame ].Time );
+//		}
+//	}	
+//}
 // - ------------------------------------------------------------------------------------------ - //
 
 // - ------------------------------------------------------------------------------------------ - //
@@ -66,63 +68,63 @@ cBody2DPose* cDynamicComponentAnimator::GetPose( const int _Animation, const int
 }
 // - ------------------------------------------------------------------------------------------ - //
 
-// - ------------------------------------------------------------------------------------------ - //
-void cDynamicComponentAnimator::SetAnimation( const int AnimationNumber, const Real& _PlayBackRate ) {
-	// Set the animation as requested //
-	if ( AnimationNumber < (int)AnimationSet->Animation.size() )
-		CurrentAnimation = AnimationNumber;
-	else {
-		Log( 10, "Error!  Animation " << AnimationNumber << " Requested, but doesn't exist!" );
-	}
-	
-	// Set internal animator variables //
-	PlayBackRate = _PlayBackRate;
-	CurrentFrame = 0;
-	CurrentFrameTime = Real::Zero;
-	
-	// Clear Flags //
-	AnimationFlags.Clear();
-	
-	// Set Pose //
-//	Body.Pose = &AnimationSet->BodyPose[ AnimationSet->Animation[ CurrentAnimation ].Frame[ CurrentFrame ].BodyPoseIndex ];
-}
-// - ------------------------------------------------------------------------------------------ - //
-void cDynamicComponentAnimator::SetArcingAnimation( const int AnimationNumber ) {
-	// Set the animation as requested //
-	if ( AnimationNumber < (int)AnimationSet->Animation.size() )
-		CurrentAnimation = AnimationNumber;
-	else {
-		Log( 10, "Error!  Arcing Animation " << AnimationNumber << " Requested, but doesn't exist!" );
-	}
-	
-	// Set internal animator variables //
-	PlayBackRate = Real::Zero;
-	CurrentFrame = 0;
-	CurrentFrameTime = Real::Zero;
-	
-	// Clear Flags //
-	AnimationFlags.Clear();
-	
-	// Set Pose //
-//	Body.Pose = &AnimationSet->BodyPose[ AnimationSet->Animation[ CurrentAnimation ].Frame[ CurrentFrame ].BodyPoseIndex ];	
-}
-// - ------------------------------------------------------------------------------------------ - //
-void cDynamicComponentAnimator::SetArc( const Real& Arc ) {
-	CurrentFrame = (int)( Arc * Real( AnimationSet->Animation[ CurrentAnimation ].Frame.size() ) );
-
-	// If too big //
-	if ( CurrentFrame >= (int)AnimationSet->Animation[ CurrentAnimation ].Frame.size() ) {
-		// Clip to the end //
-		CurrentFrame = AnimationSet->Animation[ CurrentAnimation ].Frame.size() - 1;
-	}
-	// If too small //
-	else if ( CurrentFrame < 0 ) {
-		CurrentFrame = 0;
-	}
-
-	// Set Pose //
-//	Body.Pose = &AnimationSet->BodyPose[ AnimationSet->Animation[ CurrentAnimation ].Frame[ CurrentFrame ].BodyPoseIndex ];	
-}
+//// - ------------------------------------------------------------------------------------------ - //
+//void cDynamicComponentAnimator::SetAnimation( const int AnimationNumber, const Real& _PlayBackRate ) {
+//	// Set the animation as requested //
+//	if ( AnimationNumber < (int)AnimationSet->Animation.size() )
+//		CurrentAnimation = AnimationNumber;
+//	else {
+//		Log( 10, "Error!  Animation " << AnimationNumber << " Requested, but doesn't exist!" );
+//	}
+//	
+//	// Set internal animator variables //
+//	PlayBackRate = _PlayBackRate;
+//	CurrentFrame = 0;
+//	CurrentFrameTime = Real::Zero;
+//	
+//	// Clear Flags //
+//	AnimationFlags.Clear();
+//	
+//	// Set Pose //
+////	Body.Pose = &AnimationSet->BodyPose[ AnimationSet->Animation[ CurrentAnimation ].Frame[ CurrentFrame ].BodyPoseIndex ];
+//}
+//// - ------------------------------------------------------------------------------------------ - //
+//void cDynamicComponentAnimator::SetArcingAnimation( const int AnimationNumber ) {
+//	// Set the animation as requested //
+//	if ( AnimationNumber < (int)AnimationSet->Animation.size() )
+//		CurrentAnimation = AnimationNumber;
+//	else {
+//		Log( 10, "Error!  Arcing Animation " << AnimationNumber << " Requested, but doesn't exist!" );
+//	}
+//	
+//	// Set internal animator variables //
+//	PlayBackRate = Real::Zero;
+//	CurrentFrame = 0;
+//	CurrentFrameTime = Real::Zero;
+//	
+//	// Clear Flags //
+//	AnimationFlags.Clear();
+//	
+//	// Set Pose //
+////	Body.Pose = &AnimationSet->BodyPose[ AnimationSet->Animation[ CurrentAnimation ].Frame[ CurrentFrame ].BodyPoseIndex ];	
+//}
+//// - ------------------------------------------------------------------------------------------ - //
+//void cDynamicComponentAnimator::SetArc( const Real& Arc ) {
+//	CurrentFrame = (int)( Arc * Real( AnimationSet->Animation[ CurrentAnimation ].Frame.size() ) );
+//
+//	// If too big //
+//	if ( CurrentFrame >= (int)AnimationSet->Animation[ CurrentAnimation ].Frame.size() ) {
+//		// Clip to the end //
+//		CurrentFrame = AnimationSet->Animation[ CurrentAnimation ].Frame.size() - 1;
+//	}
+//	// If too small //
+//	else if ( CurrentFrame < 0 ) {
+//		CurrentFrame = 0;
+//	}
+//
+//	// Set Pose //
+////	Body.Pose = &AnimationSet->BodyPose[ AnimationSet->Animation[ CurrentAnimation ].Frame[ CurrentFrame ].BodyPoseIndex ];	
+//}
 // - ------------------------------------------------------------------------------------------ - //
 
 // - ------------------------------------------------------------------------------------------ - //
