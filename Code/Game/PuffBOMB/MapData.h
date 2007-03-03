e14aebe38f2d0f9f8adeceae4c16f1f897bf580a
rich <rich@d14347b8-48fd-0310-9e84-97907d434d85>
2007-03-03 02:36:09 +0000

    added mapdata and savedata structures that will be used for saving the game.
    git-svn-id: svn://10.0.50.55/puffbomb/trunk@2342 d14347b8-48fd-0310-9e84-97907d434d85

---
+++ Code/Game/PuffBOMB/MapData.h
@@ -0,0 +1,42 @@
+// - ------------------------------------------------------------------------------------------ - //
+// MapData //
+// - ------------------------------------------------------------------------------------------ - //
+#ifndef __Game_PuffBOMB_MapData_H__
+#define __Game_PuffBOMB_MapData_H__
+// - ------------------------------------------------------------------------------------------ - //
+#include <string>
+// - ------------------------------------------------------------------------------------------ - //
+#include <Geometry/Real.h>
+// - ------------------------------------------------------------------------------------------ - //
+class cMapData
+{
+public:
+	std::string MapName;
+	bool NormalCompleted;
+	bool AlternateCompleted;
+	bool Locked;
+	
+public:
+	// Best Time Replay
+	// Alternate Replay
+	// Prefered Replay
+	
+	Real BestTime;
+	Real AlternateTime;
+
+public:
+	cMapData( const std::string& _MapName ) :
+		MapName( _MapName ), 
+		NormalCompleted( false ),
+		AlternateCompleted( false ),
+		Locked( true ),
+		BestTime( Real::Zero ),
+		AlternateTime( Real::Zero )
+	{
+		
+	}
+			
+};
+// - ------------------------------------------------------------------------------------------ - //
+#endif // __MapData_H__ // 
+// - ------------------------------------------------------------------------------------------ - //
