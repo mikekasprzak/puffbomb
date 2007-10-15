7eae3c25c60473a1fef0887354bec643f563363d
mike <mike@d14347b8-48fd-0310-9e84-97907d434d85>
2007-10-15 18:55:03 +0000

    Added possibly DataBlockCompression LZMA function.
    git-svn-id: svn://10.0.50.55/puffbomb/trunk@2811 d14347b8-48fd-0310-9e84-97907d434d85

--- Code/Library/FileIO/DataBlockCompression.h
+++ Code/Library/FileIO/DataBlockCompression.h
@@ -5,9 +5,57 @@
 #define __Library_FileIO_DataBlockCompression_H__
 // - ------------------------------------------------------------------------------------------ - //
 #include "DataBlock.h"
+#include <External/Lzma/LzmaDecode.h>
 // - ------------------------------------------------------------------------------------------ - //
 namespace IO {
 // - ------------------------------------------------------------------------------------------ - //
+// Decode packed LZMA data to a new DataBlock //
+inline DataBlock* unpack_LZMA_DataBlock( const DataBlock* _Src ) {
+	unsigned char LZMAProperties[LZMA_PROPERTIES_SIZE];
+	// Copy first byte of LZMA Properties? //
+	*LZMAProperties = _Src->Data[0];
+	
+	// Get the Uncompressed Size from the properties //
+	unsigned long long int Size = *((unsigned long long int*)&_Src->Data[LZMA_PROPERTIES_SIZE]);
+	
+	// Decoder State structure //
+	CLzmaDecoderState state;
+	
+	// Decode the properties //
+	if( LzmaDecodeProperties( &state.Properties, LZMAProperties, LZMA_PROPERTIES_SIZE) != LZMA_RESULT_OK ) {
+		//Log( LOG_MESH_INFO, "Incorrect stream properties (LZMA.h)" );
+ 	  	return 0;
+    }
+	
+	// Allocate memory for "Probs" //
+	state.Probs = (CProb *)malloc(LzmaGetNumProbs(&state.Properties) * sizeof(CProb));
+	if (state.Probs == 0) {
+		//Log( LOG_MESH_INFO, "state.Probs == 0 (LZMA.h)" );
+		return 0;
+	}
+	
+	// Allocate a new DataBlock for our uncompressed Data //
+	DataBlock* UBuffer = new_DataBlock( Size );
+
+	// Temporary variables modified by decode function, so to learn about how it went //
+	size_t inProcessed = 0;
+	size_t outProcessed = 0;
+	
+	// Decode the file //
+	int Result = LzmaDecode( 
+		&state,
+		( unsigned char* )&_Src->Data[ LZMA_PROPERTIES_SIZE + 8 ],
+		_Src->Size,
+		&inProcessed,
+		( unsigned char* )UBuffer->Data,
+		UBuffer->Size,
+		&outProcessed
+		);
+	// TODO: React to result of the decode //
+	
+	// Return our new LZMA decompressed data //
+	return UBuffer;
+}
 // - ------------------------------------------------------------------------------------------ - //
 
 // - ------------------------------------------------------------------------------------------ - //
