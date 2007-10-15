175a10dcc569a4603f07a7e3a4681326c1638e3f
mike <mike@d14347b8-48fd-0310-9e84-97907d434d85>
2007-10-15 17:08:39 +0000

    DataBlock is tested and functional.  Also added DataBlockCompresson stub header file, for when I add compression code.
    git-svn-id: svn://10.0.50.55/puffbomb/trunk@2805 d14347b8-48fd-0310-9e84-97907d434d85

--- Code/Library/FileIO/DataBlock.h
+++ Code/Library/FileIO/DataBlock.h
@@ -4,6 +4,9 @@
 #ifndef __Library_FileIO_DataBlock_H__
 #define __Library_FileIO_DataBlock_H__
 // - ------------------------------------------------------------------------------------------ - //
+#include <cstring>
+#include <cstdio>
+// - ------------------------------------------------------------------------------------------ - //
 namespace IO {
 // - ------------------------------------------------------------------------------------------ - //
 typedef struct DataBlock {
@@ -13,23 +16,23 @@ typedef struct DataBlock {
 // - ------------------------------------------------------------------------------------------ - //
 
 // - ------------------------------------------------------------------------------------------ - //
-inline DataBlock* new_DataBlock( size_t _Size ) {
-	DataBlock* p = (DataBlock*)(new char[_Size+4]);
-	//DataBlock* p = reinterpret_cast<DataBlock*>(new char[_Size+4]);
+inline DataBlock* new_DataBlock( const size_t _Size ) {
+	//DataBlock* p = (DataBlock*)(new char[_Size+4]);
+	DataBlock* p = reinterpret_cast<DataBlock*>(new char[_Size+4]);
 	p->Size = _Size;
 	return p;
 }
 // - ------------------------------------------------------------------------------------------ - //
 inline void delete_DataBlock( DataBlock* p ) {
-	delete [] (char*)p;
-	//delete [] reinterpret_cast<char*>p;
+	//delete [] (char*)p;
+	delete [] reinterpret_cast<char*>(p);
 }
 // - ------------------------------------------------------------------------------------------ - //
 
 // - ------------------------------------------------------------------------------------------ - //
 // Use this alternative "new" function when you want to initialize the block to a value //
 // - ------------------------------------------------------------------------------------------ - //
-inline DataBlock* new_DataBlock( size_t _Size, int _InitValue ) {
+inline DataBlock* new_DataBlock( const size_t _Size, const int _InitValue ) {
 	// Allocate it //
 	DataBlock* NewBlock = new_DataBlock( _Size );
 	
@@ -42,19 +45,37 @@ inline DataBlock* new_DataBlock( size_t _Size, int _InitValue ) {
 // - ------------------------------------------------------------------------------------------ - //
 // Use this alternative "new" function when you don't know how big a file is //
 // - ------------------------------------------------------------------------------------------ - //
-inline DataBlock* new_DataBlock( char* _FileName ) {
+inline DataBlock* new_DataBlock( const char* _FileName ) {
 	// Open File //
+	FILE* fp = fopen( _FileName, "rb" );
+	if ( fp == 0 ) {
+		return 0;
+	}
 	
 	// Determine how large file is //
-	size_t Size = 0;
+	fseek( fp, 0, SEEK_END );
+	size_t Size = ftell( fp );
+	rewind( fp );
 	
 	// Allocate space //
 	DataBlock* p = new_DataBlock( Size );
+	// TODO: Assert failure allocating block //
 	
 	// Read data //
+	fread( p->Data, 1, Size, fp );
 	
+	// Close file //
+	fclose( fp );
+	
+	// Return data //
 	return p;
 }
+// - ------------------------------------------------------------------------------------------ - //
+// Variation for C++ Strings //
+// - ------------------------------------------------------------------------------------------ - //
+//inline DataBlock* new_DataBlock( const string _FileName ) {
+//	return new_DataBlock( _FileName.c_str() );
+//}
 // - ------------------------------------------------------------------------------------------ - //
 
 
@@ -63,7 +84,7 @@ inline DataBlock* new_DataBlock( char* _FileName ) {
 // TODO: Add file offsetting as optional 3rd argument (defalts to 0) //
 // TODO: Or, add a "file" type that you can construct at the top of a function, and pass to these //
 // - ------------------------------------------------------------------------------------------ - //
-inline void read_DataBlock( DataBlock* p, char* _FileName ) {
+inline void read_DataBlock( DataBlock* p, const char* _FileName ) {
 	// Should I "false" resize it? //
 	// I shouldn't need to, since I'm better off using read allocator. //
 	
@@ -71,7 +92,7 @@ inline void read_DataBlock( DataBlock* p, char* _FileName ) {
 	
 }
 // - ------------------------------------------------------------------------------------------ - //
-inline void write_DataBlock( DataBlock* p, char* _FileName ) {
+inline void write_DataBlock( DataBlock* p, const char* _FileName ) {
 	
 	// return bytes written? //
 	
@@ -79,36 +100,6 @@ inline void write_DataBlock( DataBlock* p, char* _FileName ) {
 // - ------------------------------------------------------------------------------------------ - //
 
 
-// - ------------------------------------------------------------------------------------------ - //
-// A useful accessory, resizing. //
-// Reducing size can work safely, but expanding would require new allocation //
-//   this is because new/delete ignore my internal size variable //
-// - ------------------------------------------------------------------------------------------ - //
-inline void resize_DataBlock( DataBlock** p, size_t _NewSize ) {
-	// A cheat.  We can resize the block without reallocating
-	if ( _NewSize <= (*p)->Size ) {
-		// Set the size to the new size, and we're done //
-		(*p)->Size = _NewSize;
-	}
-	else {
-		// Well, we tried.  We need to reallocate and copy the data. //
-		reallocate_DataBlock( p, _NewSize );
-	}
-}
-// - ------------------------------------------------------------------------------------------ - //
-inline void reallocate_DataBlock( DataBlock** p, size_t _NewSize ) {
-	// Allocate our new block //
-	DataBlock* NewBlock = new_DataBlock( _NewSize );
-	
-	// Copy the data to our new block //
-	copy_DataBlock( *p, NewBlock );
-	
-	// Delete the old block ponted to //
-	delete_DataBlock( *p );
-	
-	// Make the pointer point to the new block //
-	(*p) = NewBlock;
-}
 // - ------------------------------------------------------------------------------------------ - //
 // Copy one DataBlock to another, no larger than Destination Size Bytes //
 // - ------------------------------------------------------------------------------------------ - //
@@ -124,28 +115,32 @@ inline void copy_DataBlock( DataBlock* _Src, DataBlock* _Dest ) {
 	}
 }
 // - ------------------------------------------------------------------------------------------ - //
-
+// Return a duplicate of a DataBlock //
 // - ------------------------------------------------------------------------------------------ - //
-// Initializer capable versions of resize, reallocate, and copy //
-// - ------------------------------------------------------------------------------------------ - //
-inline void resize_DataBlock( DataBlock** p, size_t _NewSize, int _InitValue ) {
-	// A cheat.  We can resize the block without reallocating
-	if ( _NewSize <= (*p)->Size ) {
-		// Set the size to the new size, and we're done //
-		(*p)->Size = _NewSize;
-	}
-	else {
-		// Well, we tried.  We need to reallocate and copy the data. //
-		reallocate_DataBlock( p, _NewSize, _InitValue );
-	}
+inline DataBlock* copy_DataBlock( DataBlock* _Src ) {
+	// Allocate our new block //
+	DataBlock* NewBlock = new_DataBlock( _Src->Size );
+	
+	// Copy the data to our new block //
+	copy_DataBlock( _Src, NewBlock );
+	
+	// Return the block //
+	return NewBlock;
 }
 // - ------------------------------------------------------------------------------------------ - //
-inline void reallocate_DataBlock( DataBlock** p, size_t _NewSize, int _InitValue ) {
+
+
+// - ------------------------------------------------------------------------------------------ - //
+// A useful accessory, resizing. //
+// Reducing size can work safely, but expanding would require new allocation //
+//   this is because new/delete ignore my internal size variable //
+// - ------------------------------------------------------------------------------------------ - //
+inline void reallocate_DataBlock( DataBlock** p, const size_t _NewSize ) {
 	// Allocate our new block //
 	DataBlock* NewBlock = new_DataBlock( _NewSize );
 	
 	// Copy the data to our new block //
-	copy_DataBlock( *p, NewBlock, _InitValue );
+	copy_DataBlock( *p, NewBlock );
 	
 	// Delete the old block ponted to //
 	delete_DataBlock( *p );
@@ -154,8 +149,24 @@ inline void reallocate_DataBlock( DataBlock** p, size_t _NewSize, int _InitValue
 	(*p) = NewBlock;
 }
 // - ------------------------------------------------------------------------------------------ - //
+inline void resize_DataBlock( DataBlock** p, const size_t _NewSize ) {
+	// A cheat.  We can resize the block without reallocating
+	if ( _NewSize <= (*p)->Size ) {
+		// Set the size to the new size, and we're done //
+		(*p)->Size = _NewSize;
+	}
+	else {
+		// Well, we tried.  We need to reallocate and copy the data. //
+		reallocate_DataBlock( p, _NewSize );
+	}
+}
+// - ------------------------------------------------------------------------------------------ - //
+
+// - ------------------------------------------------------------------------------------------ - //
+// Initializer capable versions of resize, reallocate, and copy //
+// - ------------------------------------------------------------------------------------------ - //
 // Copy one DataBlock to another, no larger than Destination Size Bytes //
-inline void copy_DataBlock( DataBlock* _Src, DataBlock* _Dest, int _InitValue ) {
+inline void copy_DataBlock( DataBlock* _Src, DataBlock* _Dest, const int _InitValue ) {
 	// If source is smaller than the destination //
 	if ( _Dest->Size > _Src->Size ) {
 		// Copy source number of bytes //
@@ -169,8 +180,86 @@ inline void copy_DataBlock( DataBlock* _Src, DataBlock* _Dest, int _InitValue ) 
 	}
 }
 // - ------------------------------------------------------------------------------------------ - //
+inline void reallocate_DataBlock( DataBlock** p, size_t _NewSize, const int _InitValue ) {
+	// Allocate our new block //
+	DataBlock* NewBlock = new_DataBlock( _NewSize );
+	
+	// Copy the data to our new block //
+	copy_DataBlock( *p, NewBlock, _InitValue );
+	
+	// Delete the old block ponted to //
+	delete_DataBlock( *p );
+	
+	// Make the pointer point to the new block //
+	(*p) = NewBlock;
+}
+// - ------------------------------------------------------------------------------------------ - //
+inline void resize_DataBlock( DataBlock** p, size_t _NewSize, const int _InitValue ) {
+	// A cheat.  We can resize the block without reallocating
+	if ( _NewSize <= (*p)->Size ) {
+		// Set the size to the new size, and we're done //
+		(*p)->Size = _NewSize;
+	}
+	else {
+		// Well, we tried.  We need to reallocate and copy the data. //
+		reallocate_DataBlock( p, _NewSize, _InitValue );
+	}
+}
+// - ------------------------------------------------------------------------------------------ - //
 
 
+
+// - ------------------------------------------------------------------------------------------ - //
+// Class version of DataBlock. Deletes self as it goes out of scope. //
+// - ------------------------------------------------------------------------------------------ - //
+class cDataBlock {
+	DataBlock* _Data;
+	
+public:
+	// Constructor //
+	inline cDataBlock() :
+		_Data( 0 )
+	{
+	}
+	
+	inline cDataBlock( const size_t _Size, const int _InitValue = 0 ) :
+		_Data( new_DataBlock( _Size, _InitValue ) )
+	{
+	}
+	
+	inline cDataBlock( const char* _FileName ) :
+		_Data( new_DataBlock( _FileName ) )
+	{
+	}
+	
+	// Destructor //
+	inline ~cDataBlock() {
+		if (_Data)
+			delete_DataBlock( _Data );
+	}
+public:
+	inline size_t Size() const {
+		if (_Data)
+			return _Data->Size;
+		return 0;
+	}
+	
+	inline const char* Data() const {
+		if (_Data)
+			return _Data->Data;
+		return 0;
+	}
+	
+	inline char& operator [] ( const size_t Index ) {
+		// TODO: Assert indexing in to valid memory, and within size? //
+		return _Data->Data[ Index ];
+	}
+public:
+	inline void Resize( const size_t _Size ) {
+		if (_Data)
+			resize_DataBlock( &_Data, _Size );
+	}
+};
 // - ------------------------------------------------------------------------------------------ - //
 }; // namespace IO //
 // - ------------------------------------------------------------------------------------------ - //
