26876592ea718c62aaa64c47f778c64042d9d403
mike <mike@d14347b8-48fd-0310-9e84-97907d434d85>
2007-10-16 17:32:58 +0000

    I started toying with adding all the compression support to the "non DataBlock" portion of the FileIO library, but I got bored of it.  I don't need it right now anyways.  Next thing.
    git-svn-id: svn://10.0.50.55/puffbomb/trunk@2825 d14347b8-48fd-0310-9e84-97907d434d85

--- Code/Library/FileIO/DataUtilCompression.h
+++ Code/Library/FileIO/DataUtilCompression.h
@@ -126,6 +126,193 @@ inline const size_t unpack_LZMA_Data( const void* _Src, const size_t _Size, cons
 // - ------------------------------------------------------------------------------------------ - //
 
 
+//// - ------------------------------------------------------------------------------------------ - //
+//// Decode packed BZIP data to a new DataBlock //
+//inline const size_t unpack_BZIP_Data( const void* _Src, const size_t _Size, const void** _Dest, const size_t _DestSize ) {
+//	// Assume the compressed DataBlock contains a DataBlock, who's size is the uncompressed size //
+//	DataBlock* Compressed = reinterpret_cast<DataBlock*>(_Src->Data);
+//	
+//	// Allocate a memory block equal to the uncompressed size //
+//	DataBlock* Uncompressed = new_DataBlock( Compressed->Size );
+//	
+//	// Decompress the Data //
+//	int Error = BZ2_bzBuffToBuffDecompress(
+//		Uncompressed->Data,
+//		&Uncompressed->Size,
+//		Compressed->Data, // Compressed Data (After Size, and Uncompressed Size (8 bytes)) //
+//		_Src->Size - 4, // Source size, since we have an extra 4 byte uncompressed size //
+//		0, // small (0 - more memory used, faster. 1 - less memory (2300k max), slower) //
+//		0 // verbosity (0, don't talk) //
+//		);
+//	// TODO: Assert on uncompress error //
+//	//if ( Error != BZ_OK )
+//	
+//	// Return the uncompressed buffer //
+//	return Uncompressed;
+//}
+//// - ------------------------------------------------------------------------------------------ - //
+//// Encode packed BZIP data to a new DataBlock //
+//inline DataBlock* pack_BZIP_Data( const void* _Src, const size_t _Size, const void** _Dest, const size_t _DestSize ) {
+//	// Allocate worst case space to store compressed data //
+//	DataBlock* Compressed = new_DataBlock( _Src->Size + (_Src->Size / 100) + 600 + 4 );
+//	
+//	size_t* UncompressedSize = reinterpret_cast<size_t*>(&Compressed->Data[0]);
+//	*UncompressedSize = 0;
+//	size_t CompressedSize = Compressed->Size-4;
+//	
+//	// Compress the Data //
+//	int Error = BZ2_bzBuffToBuffCompress(
+//		(char*)(&(Compressed->Data[4])),
+//		&CompressedSize,
+//		(char*)_Src->Data,
+//		_Src->Size,
+//		9, // blockSize100k (value * 100000 bytes allocated for work spcae (1-9)) //
+//		0, // verbosity (0, don't talk) //
+//		0 // workFactor (0 defaults to recommended case, 30) //
+//		);
+//	// TODO: Assert on compress error //
+//	//if ( Error != BZ_OK )
+//
+//	
+//	// Store Sizes //
+//	Compressed->Size = CompressedSize + 4;
+//	*UncompressedSize = _Src->Size;
+//
+//	// If bzip worked, bzip will have changed the size in the DataBlock to less than the //
+//	//   memory allocated.  Thusly, we can call reallocate to force the data buffer to be //
+//	//   re-allocated to reduce memory usage (causing normal OS fragmentation). //
+//	reallocate_DataBlock( &Compressed );
+//	
+//	// Return the compressed buffer //
+//	return Compressed;
+//}
+//// - ------------------------------------------------------------------------------------------ - //
+//
+//
+//// - ------------------------------------------------------------------------------------------ - //
+//// Decode packed ZLIB data to a new DataBlock //
+//inline DataBlock* unpack_ZLIB_Data( void* _Src ) {
+//	// Assume the compressed DataBlock contains a DataBlock, who's size is the uncompressed size //
+//	DataBlock* Compressed = reinterpret_cast<DataBlock*>(_Src->Data);
+//	
+//	// Allocate a memory block equal to the uncompressed size //
+//	DataBlock* Uncompressed = new_DataBlock( Compressed->Size );
+//	
+//	// Decompress the Data //
+//	int Error = uncompress(
+//		(Bytef*)Uncompressed->Data,
+//		(uLongf*)&Uncompressed->Size,
+//		(const Bytef*)Compressed->Data, // Compressed Data (After Size, and Uncompressed Size (8 bytes)) //
+//		_Src->Size - 4 // Source size, since we have an extra 4 byte uncompressed size //
+//		);
+//	// TODO: Assert on uncompress error //
+//	//if ( Error != Z_OK )
+//	
+//	// Return the uncompressed buffer //
+//	return Uncompressed;
+//}
+//// - ------------------------------------------------------------------------------------------ - //
+//// Encode packed ZLIB data to a new DataBlock //
+//inline DataBlock* pack_ZLIB_Data( void* _Src ) {
+//	// Allocate worst case space to store compressed data //
+//	DataBlock* Compressed = new_DataBlock( _Src->Size + (_Src->Size / 1000) + 12 + 4 );
+//	
+//	size_t* UncompressedSize = reinterpret_cast<size_t*>(&Compressed->Data[0]);
+//	*UncompressedSize = 0;
+//	size_t CompressedSize = Compressed->Size-4;
+//	
+//	// Compress the Data //
+//	int Error = compress(
+//		(Bytef*)(&(Compressed->Data[4])),
+//		(uLongf*)&CompressedSize,
+//		(const Bytef*)_Src->Data,
+//		_Src->Size
+//		//Z_DEFAULT_COMPRESSION // Compression level.  1 best - 9 worst. 0 none. default is 6 //
+//		);
+//	// TODO: Assert on compress error //
+//	//if ( Error != Z_OK )
+//	
+//	// Store Sizes //
+//	Compressed->Size = CompressedSize + 4;
+//	*UncompressedSize = _Src->Size;
+//
+//	// If zlib worked, zlib will have changed the size in the DataBlock to less than the //
+//	//   memory allocated.  Thusly, we can call reallocate to force the data buffer to be //
+//	//   re-allocated to reduce memory usage (causing normal OS fragmentation). //
+//	reallocate_DataBlock( &Compressed );
+//	
+//	// Return the compressed buffer //
+//	return Compressed;
+//}
+//// - ------------------------------------------------------------------------------------------ - //
+//
+//
+//// - ------------------------------------------------------------------------------------------ - //
+//// NOTE: LZO compression should have and use static work memory.  Allocation slows it down. //
+//// - ------------------------------------------------------------------------------------------ - //
+//// Decode packed LZO data to a new DataBlock //
+//inline DataBlock* unpack_LZO_Data( void* _Src ) {
+//	// Assume the compressed DataBlock contains a DataBlock, who's size is the uncompressed size //
+//	DataBlock* Compressed = reinterpret_cast<DataBlock*>(_Src->Data);
+//	
+//	// Allocate a memory block equal to the uncompressed size //
+//	DataBlock* Uncompressed = new_DataBlock( Compressed->Size );
+//	
+//	// Decompress the Data //
+//	int Error = lzo1x_decompress(
+//		(const lzo_bytep)Compressed->Data, // Compressed Data (After Size, and Uncompressed Size (8 bytes)) //
+//		_Src->Size - 4, // Source size, since we have an extra 4 byte uncompressed size //
+//		(lzo_bytep)Uncompressed->Data,
+//		(lzo_uintp)&Uncompressed->Size,
+//		(lzo_voidp)0 // No work memory needed //
+//		);
+//	// TODO: Assert on uncompress error //
+//	//if ( Error != Z_OK )
+//	
+//	// Return the uncompressed buffer //
+//	return Uncompressed;
+//}
+//// - ------------------------------------------------------------------------------------------ - //
+//// Encode packed ZLIB data to a new DataBlock //
+//inline DataBlock* pack_LZO_Data( void* _Src ) {
+//	// Allocate worst case space to store compressed data //
+//	DataBlock* Compressed = new_DataBlock( _Src->Size + (_Src->Size / 1000) + 12 + 4 );
+//	
+//	size_t* UncompressedSize = reinterpret_cast<size_t*>(&Compressed->Data[0]);
+//	*UncompressedSize = 0;
+//	size_t CompressedSize = Compressed->Size-4;
+//	
+//	// Note: BAD!  Should not allocate here, should pre allocate these! //
+//	char* WorkMemory = new char[LZO1X_1_MEM_COMPRESS]; 
+//	
+//	// Compress the Data //
+//	int Error = lzo1x_1_compress(
+//		(const lzo_bytep)_Src->Data,
+//		_Src->Size,
+//		(lzo_bytep)(&(Compressed->Data[4])),
+//		(lzo_uintp)&CompressedSize,
+//		(lzo_voidp)WorkMemory
+//		);
+//	// TODO: Assert on compress error //
+//	//if ( Error != Z_OK )
+//	
+//	// Note: BAD!  Should not have to free here! //
+//	delete [] WorkMemory;
+//	
+//	// Store Sizes //
+//	Compressed->Size = CompressedSize + 4;
+//	*UncompressedSize = _Src->Size;
+//
+//	// If zlib worked, zlib will have changed the size in the DataBlock to less than the //
+//	//   memory allocated.  Thusly, we can call reallocate to force the data buffer to be //
+//	//   re-allocated to reduce memory usage (causing normal OS fragmentation). //
+//	reallocate_DataBlock( &Compressed );
+//	
+//	// Return the compressed buffer //
+//	return Compressed;
+//}
+//// - ------------------------------------------------------------------------------------------ - //
+
 // - ------------------------------------------------------------------------------------------ - //
 }; // namespace IO //
 // - ------------------------------------------------------------------------------------------ - //
