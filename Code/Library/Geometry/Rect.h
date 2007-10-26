fff34e664c05305a299dd242ef91150d6c571a56
mike <mike@d14347b8-48fd-0310-9e84-97907d434d85>
2007-10-26 23:35:24 +0000

    Restored old rect.h file.
    git-svn-id: svn://10.0.50.55/puffbomb/trunk@2827 d14347b8-48fd-0310-9e84-97907d434d85

--- Code/Library/Geometry/Rect.h
+++ Code/Library/Geometry/Rect.h
@@ -10,9 +10,9 @@
 #include "Rect/RadiusRect2D.h"
 #include "Rect/PairRect2D.h"
 
-//#include "Rect/Rect3D.h"
-//#include "Rect/RadiusRect3D.h"
-//#include "Rect/PairRect3D.h"
+#include "Rect/Rect3D.h"
+#include "Rect/RadiusRect3D.h"
+#include "Rect/PairRect3D.h"
 // - ------------------------------------------------------------------------------------------ - //
 
 // - ------------------------------------------------------------------------------------------ - //
@@ -160,161 +160,161 @@ const RadiusRect2D PairRect2D::ToRadiusRect() const {
 // - ------------------------------------------------------------------------------------------ - //
 
 
-//// - ------------------------------------------------------------------------------------------ - //
-//// Rect vs. Rect //
-//const bool Rect3D::operator == ( const Rect3D& Vs ) const {
-//	PairRect3D r( P1(), Shape() + Vs.Shape() );
-//	Vector3D v( Vs.P2() );
-//	
-//	if ( v.x >= r.P1().x )
-//		if ( v.x < r.P2().x )
-//			if ( v.y >= r.P1().y )
-//				if ( v.y < r.P2().y )
-//					if ( v.z >= r.P1().z )
-//						return ( v.z < r.P2().z );
-//	return false;
-//}
-//// - ------------------------------------------------------------------------------------------ - //
-//// RadiusRect3D vs. RadiusRect3D //
-//const bool RadiusRect3D::operator == ( const RadiusRect3D& Vs ) const {
-//	PairRect3D r( P1(), Shape() + Vs.Shape() );
-//	Vector3D v( Vs.P2() );
-//	
-//	if ( v.x >= r.P1().x )
-//		if ( v.x < r.P2().x )
-//			if ( v.y >= r.P1().y )
-//				if ( v.y < r.P2().y )
-//					if ( v.z >= r.P1().z )
-//						return ( v.z < r.P2().z );
-//	return false;
-//}
-//// - ------------------------------------------------------------------------------------------ - //
-//// PairRect3D vs. PairRect3D //
-//const bool PairRect3D::operator == ( const PairRect3D& Vs ) const {
-//	PairRect3D r( P1(), Shape() + Vs.Shape() );
-//	Vector3D v( Vs.P2() );
-//	
-//	if ( v.x >= r.P1().x )
-//		if ( v.x < r.P2().x )
-//			if ( v.y >= r.P1().y )
-//				if ( v.y < r.P2().y )
-//					if ( v.z >= r.P1().z )
-//						return ( v.z < r.P2().z );
-//	return false;
-//}
-//// - ------------------------------------------------------------------------------------------ - //
-//
-//
-//// - ------------------------------------------------------------------------------------------ - //
-//// Rect vs. Vector //
-//const bool Rect3D::operator == ( const Vector3D& Vs ) const {
-//	if ( Vs.x >= P1().x )
-//		if ( Vs.x < P2().x )
-//			if ( Vs.y >= P1().y )
-//				if ( Vs.y < P2().y );
-//					if ( Vs.z >= P1().z )
-//						return ( Vs.z < P2().z );
-//	return false;
-//}
-//// - ------------------------------------------------------------------------------------------ - //
-//inline const bool operator == ( const Vector3D& a, const Rect3D& b ) {
-//	return b == a;
-//}
-//// - ------------------------------------------------------------------------------------------ - //
-//inline const bool operator != ( const Rect3D& a, const Vector3D& b ) {
-//	return !(a == b);
-//}
-//// - ------------------------------------------------------------------------------------------ - //
-//inline const bool operator != ( const Vector3D& a, const Rect3D& b ) {
-//	return !(b == a);
-//}
-//// - ------------------------------------------------------------------------------------------ - //
-//// RadiusRect vs. Vector //
-//const bool RadiusRect3D::operator == ( const Vector3D& Vs ) const {
-//	if ( Vs.x >= P1().x )
-//		if ( Vs.x < P2().x )
-//			if ( Vs.y >= P1().y )
-//				if ( Vs.y < P2().y );
-//					if ( Vs.z >= P1().z )
-//						return ( Vs.z < P2().z );
-//	return false;
-//}
-//// - ------------------------------------------------------------------------------------------ - //
-//inline const bool operator == ( const Vector3D& a, const RadiusRect3D& b ) {
-//	return b == a;
-//}
-//// - ------------------------------------------------------------------------------------------ - //
-//inline const bool operator != ( const RadiusRect3D& a, const Vector3D& b ) {
-//	return !(a == b);
-//}
-//// - ------------------------------------------------------------------------------------------ - //
-//inline const bool operator != ( const Vector3D& a, const RadiusRect3D& b ) {
-//	return !(b == a);
-//}
-//// - ------------------------------------------------------------------------------------------ - //
-//// PairRect vs. Vector //
-//const bool PairRect3D::operator == ( const Vector3D& Vs ) const {
-//	if ( Vs.x >= P1().x )
-//		if ( Vs.x < P2().x )
-//			if ( Vs.y >= P1().y )
-//				if ( Vs.y < P2().y );
-//					if ( Vs.z >= P1().z )
-//						return ( Vs.z < P2().z );
-//	return false;
-//}
-//// - ------------------------------------------------------------------------------------------ - //
-//inline const bool operator == ( const Vector3D& a, const PairRect3D& b ) {
-//	return b == a;
-//}
-//// - ------------------------------------------------------------------------------------------ - //
-//inline const bool operator != ( const PairRect3D& a, const Vector3D& b ) {
-//	return !(a == b);
-//}
-//// - ------------------------------------------------------------------------------------------ - //
-//inline const bool operator != ( const Vector3D& a, const PairRect3D& b ) {
-//	return !(b == a);
-//}
-//// - ------------------------------------------------------------------------------------------ - //
-//
-//
-//
-//// - ------------------------------------------------------------------------------------------ - //
-//// Rect Conversions //
-//// - ------------------------------------------------------------------------------------------ - //
-//const RadiusRect3D Rect3D::ToRadiusRect() const {
-//	return RadiusRect3D( P1(), Shape() );
-//}
-//// - ------------------------------------------------------------------------------------------ - //
-//const PairRect3D Rect3D::ToPairRect() const {
-//	return PairRect3D( P1(), Shape() );
-//}
-//// - ------------------------------------------------------------------------------------------ - //
-//
-//
-//// - ------------------------------------------------------------------------------------------ - //
-//// RadiusRect Conversions //
-//// - ------------------------------------------------------------------------------------------ - //
-//const Rect3D RadiusRect3D::ToRect() const {
-//	return Rect3D::Radius( Center(), HalfShape() );
-//}
-//// - ------------------------------------------------------------------------------------------ - //
-//const PairRect3D RadiusRect3D::ToPairRect() const {
-//	return PairRect3D::Radius( Center(), HalfShape() );
-//}
-//// - ------------------------------------------------------------------------------------------ - //
-//
-//
-//// - ------------------------------------------------------------------------------------------ - //
-//// PairRect Conversions //
-//// - ------------------------------------------------------------------------------------------ - //
-//const Rect3D PairRect3D::ToRect() const {
-//	return Rect3D::Pair( P1(), P2() );
-//}
-//// - ------------------------------------------------------------------------------------------ - //
-//const RadiusRect3D PairRect3D::ToRadiusRect() const {
-//	return RadiusRect3D::Pair( P1(), P2() );
-//}
-//// - ------------------------------------------------------------------------------------------ - //
+// - ------------------------------------------------------------------------------------------ - //
+// Rect vs. Rect //
+const bool Rect3D::operator == ( const Rect3D& Vs ) const {
+	PairRect3D r( P1(), Shape() + Vs.Shape() );
+	Vector3D v( Vs.P2() );
+	
+	if ( v.x >= r.P1().x )
+		if ( v.x < r.P2().x )
+			if ( v.y >= r.P1().y )
+				if ( v.y < r.P2().y )
+					if ( v.z >= r.P1().z )
+						return ( v.z < r.P2().z );
+	return false;
+}
+// - ------------------------------------------------------------------------------------------ - //
+// RadiusRect3D vs. RadiusRect3D //
+const bool RadiusRect3D::operator == ( const RadiusRect3D& Vs ) const {
+	PairRect3D r( P1(), Shape() + Vs.Shape() );
+	Vector3D v( Vs.P2() );
+	
+	if ( v.x >= r.P1().x )
+		if ( v.x < r.P2().x )
+			if ( v.y >= r.P1().y )
+				if ( v.y < r.P2().y )
+					if ( v.z >= r.P1().z )
+						return ( v.z < r.P2().z );
+	return false;
+}
+// - ------------------------------------------------------------------------------------------ - //
+// PairRect3D vs. PairRect3D //
+const bool PairRect3D::operator == ( const PairRect3D& Vs ) const {
+	PairRect3D r( P1(), Shape() + Vs.Shape() );
+	Vector3D v( Vs.P2() );
+	
+	if ( v.x >= r.P1().x )
+		if ( v.x < r.P2().x )
+			if ( v.y >= r.P1().y )
+				if ( v.y < r.P2().y )
+					if ( v.z >= r.P1().z )
+						return ( v.z < r.P2().z );
+	return false;
+}
+// - ------------------------------------------------------------------------------------------ - //
+
+
+// - ------------------------------------------------------------------------------------------ - //
+// Rect vs. Vector //
+const bool Rect3D::operator == ( const Vector3D& Vs ) const {
+	if ( Vs.x >= P1().x )
+		if ( Vs.x < P2().x )
+			if ( Vs.y >= P1().y )
+				if ( Vs.y < P2().y );
+					if ( Vs.z >= P1().z )
+						return ( Vs.z < P2().z );
+	return false;
+}
+// - ------------------------------------------------------------------------------------------ - //
+inline const bool operator == ( const Vector3D& a, const Rect3D& b ) {
+	return b == a;
+}
+// - ------------------------------------------------------------------------------------------ - //
+inline const bool operator != ( const Rect3D& a, const Vector3D& b ) {
+	return !(a == b);
+}
+// - ------------------------------------------------------------------------------------------ - //
+inline const bool operator != ( const Vector3D& a, const Rect3D& b ) {
+	return !(b == a);
+}
+// - ------------------------------------------------------------------------------------------ - //
+// RadiusRect vs. Vector //
+const bool RadiusRect3D::operator == ( const Vector3D& Vs ) const {
+	if ( Vs.x >= P1().x )
+		if ( Vs.x < P2().x )
+			if ( Vs.y >= P1().y )
+				if ( Vs.y < P2().y );
+					if ( Vs.z >= P1().z )
+						return ( Vs.z < P2().z );
+	return false;
+}
+// - ------------------------------------------------------------------------------------------ - //
+inline const bool operator == ( const Vector3D& a, const RadiusRect3D& b ) {
+	return b == a;
+}
+// - ------------------------------------------------------------------------------------------ - //
+inline const bool operator != ( const RadiusRect3D& a, const Vector3D& b ) {
+	return !(a == b);
+}
+// - ------------------------------------------------------------------------------------------ - //
+inline const bool operator != ( const Vector3D& a, const RadiusRect3D& b ) {
+	return !(b == a);
+}
+// - ------------------------------------------------------------------------------------------ - //
+// PairRect vs. Vector //
+const bool PairRect3D::operator == ( const Vector3D& Vs ) const {
+	if ( Vs.x >= P1().x )
+		if ( Vs.x < P2().x )
+			if ( Vs.y >= P1().y )
+				if ( Vs.y < P2().y );
+					if ( Vs.z >= P1().z )
+						return ( Vs.z < P2().z );
+	return false;
+}
+// - ------------------------------------------------------------------------------------------ - //
+inline const bool operator == ( const Vector3D& a, const PairRect3D& b ) {
+	return b == a;
+}
+// - ------------------------------------------------------------------------------------------ - //
+inline const bool operator != ( const PairRect3D& a, const Vector3D& b ) {
+	return !(a == b);
+}
+// - ------------------------------------------------------------------------------------------ - //
+inline const bool operator != ( const Vector3D& a, const PairRect3D& b ) {
+	return !(b == a);
+}
+// - ------------------------------------------------------------------------------------------ - //
+
+
+
+// - ------------------------------------------------------------------------------------------ - //
+// Rect Conversions //
+// - ------------------------------------------------------------------------------------------ - //
+const RadiusRect3D Rect3D::ToRadiusRect() const {
+	return RadiusRect3D( P1(), Shape() );
+}
+// - ------------------------------------------------------------------------------------------ - //
+const PairRect3D Rect3D::ToPairRect() const {
+	return PairRect3D( P1(), Shape() );
+}
+// - ------------------------------------------------------------------------------------------ - //
+
+
+// - ------------------------------------------------------------------------------------------ - //
+// RadiusRect Conversions //
+// - ------------------------------------------------------------------------------------------ - //
+const Rect3D RadiusRect3D::ToRect() const {
+	return Rect3D::Radius( Center(), HalfShape() );
+}
+// - ------------------------------------------------------------------------------------------ - //
+const PairRect3D RadiusRect3D::ToPairRect() const {
+	return PairRect3D::Radius( Center(), HalfShape() );
+}
+// - ------------------------------------------------------------------------------------------ - //
+
+
+// - ------------------------------------------------------------------------------------------ - //
+// PairRect Conversions //
+// - ------------------------------------------------------------------------------------------ - //
+const Rect3D PairRect3D::ToRect() const {
+	return Rect3D::Pair( P1(), P2() );
+}
+// - ------------------------------------------------------------------------------------------ - //
+const RadiusRect3D PairRect3D::ToRadiusRect() const {
+	return RadiusRect3D::Pair( P1(), P2() );
+}
+// - ------------------------------------------------------------------------------------------ - //
 
 
 // - ------------------------------------------------------------------------------------------ - //
