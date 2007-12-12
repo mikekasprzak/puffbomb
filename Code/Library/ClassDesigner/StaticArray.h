4e78765389ec8a18b10a82799a566cabc8ff7e55
mike <mike@d14347b8-48fd-0310-9e84-97907d434d85>
2007-12-12 05:38:41 +0000

    Added new StaticArray.  This static array is for creating local scope dynamic like arrays.  You specify a max size in the template creation, and you can resize it or push back on to it as you see fit.  It doesn't yet support insertion, pushing/popping off the front, or an iterator syntax, but it's usable for linear order creation.
    git-svn-id: svn://10.0.50.55/puffbomb/trunk@2863 d14347b8-48fd-0310-9e84-97907d434d85

---
+++ Code/Library/ClassDesigner/StaticArray.h
@@ -0,0 +1,58 @@
+// - ------------------------------------------------------------------------------------------ - //
+// Statically allocated Array - Allows you to work with data like it was a dynamic array //
+// - ------------------------------------------------------------------------------------------ - //
+#ifndef __Library_ClassDesigner_StaticArray_H__
+#define __Library_ClassDesigner_StaticArray_H__
+// - ------------------------------------------------------------------------------------------ - //
+
+// - ------------------------------------------------------------------------------------------ - //
+template< class Type, size_t MaxSize >
+class StaticArray {
+	size_t CurrentSize;
+	Type Data[MaxSize];
+public:
+	inline StaticArray( const size_t _CurrentSize = 0 ) :
+		CurrentSize( _CurrentSize )
+	{
+	}
+	
+public:
+	inline const size_t Size() const {
+		return CurrentSize;
+	}
+	
+	inline Type& operator [] ( const size_t Index ) {
+		// TODO: Assert if index larger than max size (current size?) //
+		return Data[ Index ];
+	}
+
+	inline Type* operator * () {
+		return Data;
+	}
+	
+public:
+	inline void Resize( const size_t _NewSize ) {
+		// TODO: Assert if too large //
+		CurrentSize = _NewSize;
+	}
+	
+	inline void PushBack( const Type& _NewData ) {
+		Data[CurrentSize] = _NewData;
+	
+		CurrentSize++;
+		// TODO: Assert if larger than MaxSize //	
+	}
+	
+	inline Type& PopBack() {
+		CurrentSize--;
+		
+		return Data[CurrentSize];
+	}
+	
+	// TODO: Insert, PushFront, and iterators //
+};
+// - ------------------------------------------------------------------------------------------ - //
+
+// - ------------------------------------------------------------------------------------------ - //
+#endif // __Library_ClassDesigner_StaticArray_H__ //
+// - ------------------------------------------------------------------------------------------ - //
