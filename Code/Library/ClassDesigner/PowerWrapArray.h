bbb23f52b96d6938eff28e5e06747fe8b28e2018
mike <mike@d14347b8-48fd-0310-9e84-97907d434d85>
2007-12-12 06:03:25 +0000

    Added wrapping array and power wrapping array.  The power version is for situations when you can use a power of 2 sized array, and want a much faster implementation of the wrapping feature.
    git-svn-id: svn://10.0.50.55/puffbomb/trunk@2864 d14347b8-48fd-0310-9e84-97907d434d85

---
+++ Code/Library/ClassDesigner/PowerWrapArray.h
@@ -0,0 +1,77 @@
+// - ------------------------------------------------------------------------------------------ - //
+// Statically allocated Wrapping Array - Allows you to work with data like it was a dynamic array //
+// - ------------------------------------------------------------------------------------------ - //
+#ifndef __Library_ClassDesigner_PowerWrapArray_H__
+#define __Library_ClassDesigner_PowerWrapArray_H__
+// - ------------------------------------------------------------------------------------------ - //
+
+// - ------------------------------------------------------------------------------------------ - //
+// MaxSize is expected to be a power of 2.  The code uses & to wrap, so this should be very cheap //
+// - ------------------------------------------------------------------------------------------ - //
+// TODO: Investigate making this a specialization of WrapArray, when MaxSize is a Power of 2 //
+// - ------------------------------------------------------------------------------------------ - //
+template< class Type, size_t MaxSize >
+class PowerWrapArray {
+	size_t CurrentSize;
+	size_t BaseIndex;
+	Type Data[MaxSize];
+public:
+	inline StaticArray( const size_t _CurrentSize = 0 ) :
+		CurrentSize( _CurrentSize ),
+		BaseIndex( 0 )
+	{
+	}
+	
+public:
+	inline const size_t Size() const {
+		return CurrentSize;
+	}
+	
+	inline Type& operator [] ( const size_t Index ) {
+		// TODO: Assert if index larger than max size (current size?) //
+		return Data[ (BaseIndex+Index) & (MaxSize-1) ];
+	}
+	
+public:
+	inline void Resize( const size_t _NewSize ) {
+		// TODO: Assert if too large //
+		CurrentSize = _NewSize;
+	}
+	
+	inline void PushBack( const Type& _NewData ) {
+		Data[(BaseIndex+CurrentSize) & (MaxSize-1)] = _NewData;
+	
+		CurrentSize++;
+		// TODO: Assert if larger than MaxSize //	
+	}
+	
+	inline Type& PopBack() {
+		CurrentSize--;
+		
+		return Data[(BaseIndex+CurrentSize) & (MaxSize-1)];
+	}
+
+	inline void PushFront( const Type& _NewData ) {
+		BaseIndex--;
+		
+		Data[BaseIndex & (MaxSize-1)] = _NewData;
+	
+		CurrentSize++;
+		// TODO: Assert if larger than MaxSize //	
+	}
+	
+	inline Type& PopFront() {
+		CurrentSize--;
+		
+		return Data[BaseIndex & (MaxSize-1)];
+		
+		BaseIndex++;
+	}
+		
+	// TODO: Insert and iterators //
+};
+// - ------------------------------------------------------------------------------------------ - //
+
+// - ------------------------------------------------------------------------------------------ - //
+#endif // __Library_ClassDesigner_PowerWrapArray_H__ //
+// - ------------------------------------------------------------------------------------------ - //
---
+++ Code/Library/ClassDesigner/WrapArray.h
@@ -0,0 +1,73 @@
+// - ------------------------------------------------------------------------------------------ - //
+// Statically allocated Wrapping Array - Allows you to work with data like it was a dynamic array //
+// - ------------------------------------------------------------------------------------------ - //
+#ifndef __Library_ClassDesigner_WrapArray_H__
+#define __Library_ClassDesigner_WrapArray_H__
+// - ------------------------------------------------------------------------------------------ - //
+
+// - ------------------------------------------------------------------------------------------ - //
+template< class Type, size_t MaxSize >
+class WrapArray {
+	size_t CurrentSize;
+	size_t BaseIndex;
+	Type Data[MaxSize];
+public:
+	inline StaticArray( const size_t _CurrentSize = 0 ) :
+		CurrentSize( _CurrentSize ),
+		BaseIndex( 0 )
+	{
+	}
+	
+public:
+	inline const size_t Size() const {
+		return CurrentSize;
+	}
+	
+	inline Type& operator [] ( const size_t Index ) {
+		// TODO: Assert if index larger than max size (current size?) //
+		return Data[ (BaseIndex+Index) % MaxSize ];
+	}
+	
+public:
+	inline void Resize( const size_t _NewSize ) {
+		// TODO: Assert if too large //
+		CurrentSize = _NewSize;
+	}
+	
+	inline void PushBack( const Type& _NewData ) {
+		Data[(BaseIndex+CurrentSize) % MaxSize] = _NewData;
+	
+		CurrentSize++;
+		// TODO: Assert if larger than MaxSize //	
+	}
+	
+	inline Type& PopBack() {
+		CurrentSize--;
+		
+		return Data[(BaseIndex+CurrentSize) % MaxSize];
+	}
+
+	inline void PushFront( const Type& _NewData ) {
+		BaseIndex--;
+		
+		Data[BaseIndex % MaxSize] = _NewData;
+	
+		CurrentSize++;
+		// TODO: Assert if larger than MaxSize //	
+	}
+	
+	inline Type& PopFront() {
+		CurrentSize--;
+		
+		return Data[BaseIndex % MaxSize];
+		
+		BaseIndex++;
+	}
+		
+	// TODO: Insert and iterators //
+};
+// - ------------------------------------------------------------------------------------------ - //
+
+// - ------------------------------------------------------------------------------------------ - //
+#endif // __Library_ClassDesigner_WrapArray_H__ //
+// - ------------------------------------------------------------------------------------------ - //
