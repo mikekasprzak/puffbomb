dd69867b5cc134b294fe4342bd11ad4ed0644cc2
mike <mike@d14347b8-48fd-0310-9e84-97907d434d85>
2008-01-09 19:31:39 +0000

    Refactored Array_BZIP.h, and updated Array_LZMA to now use new compression methodology.
    git-svn-id: svn://10.0.50.55/puffbomb/trunk@2913 d14347b8-48fd-0310-9e84-97907d434d85

--- Code/Library/Data/Array_BZIP.h
+++ Code/Library/Data/Array_BZIP.h
@@ -4,10 +4,10 @@
 #ifndef __Library_Data_Array_BZIP_H__
 #define __Library_Data_Array_BZIP_H__
 // - ------------------------------------------------------------------------------------------ - //
-#include "Array.h"
 #include "DataBlock_Core.h"
 #include "DataBlock_BZIP.h"
-//#include <External/BZip/bzlib.h>
+
+#include "Array_Core.h"
 // - ------------------------------------------------------------------------------------------ - //
 //namespace Data {
 // - ------------------------------------------------------------------------------------------ - //
@@ -15,61 +15,18 @@
 template< class Type >
 inline Array<Type>* unpack_BZIP_Array( DataBlock* _Src ) {
 	DataBlock* Uncompressed = unpack_BZIP_DataBlock( _Src );
-	
 	Array<Type>* UncompressedArray = new_Array<Type>( Uncompressed );
 	
 	delete_DataBlock( Uncompressed );
 	
 	return UncompressedArray;
-	
-	
-	
-//	// Assume the compressed Array contains a Array, who's size is the uncompressed size //
-//	DataBlock* Compressed = reinterpret_cast<DataBlock*>(_Src->Data);
-//	
-//	// Allocate a memory block equal to the uncompressed size //
-//	Array<Type>* Uncompressed = new_Array<Type>( Compressed->Size / sizeof(Type) );
-//	
-//	// Decompress the Data //
-//	int Error = BZ2_bzBuffToBuffDecompress(
-//		(char*)Uncompressed->Data,
-//		&Uncompressed->Size,
-//		Compressed->Data, // Compressed Data (After Size, and Uncompressed Size (8 bytes)) //
-//		_Src->Size - 4, // Source size, since we have an extra 4 byte uncompressed size //
-//		0, // small (0 - more memory used, faster. 1 - less memory (2300k max), slower) //
-//		0 // verbosity (0, don't talk) //
-//		);
-//	// TODO: Assert on uncompress error //
-//	//if ( Error != BZ_OK )
-//	
-//	// Return the uncompressed buffer //
-//	return Uncompressed;
 }
-//// - ------------------------------------------------------------------------------------------ - //
-//// Decode packed BZIP data to a new Array //
-//template< class Type >
-//inline Array<Type>* unpack_BZIP_Array( char* _Src, const size_t _Size ) {
-//	// Assume the compressed Array contains a Array, who's size is the uncompressed size //
-//	DataBlock* Compressed = reinterpret_cast<DataBlock*>(_Src);
-//	
-//	// Allocate a memory block equal to the uncompressed size //
-//	Array<Type>* Uncompressed = new_Array<Type>( Compressed->Size / sizeof(Type) );
-//	
-//	// Decompress the Data //
-//	int Error = BZ2_bzBuffToBuffDecompress(
-//		(char*)Uncompressed->Data,
-//		&Uncompressed->Size,
-//		Compressed->Data, // Compressed Data (After Size, and Uncompressed Size (8 bytes)) //
-//		_Size - 4, // Source size, since we have an extra 4 byte uncompressed size //
-//		0, // small (0 - more memory used, faster. 1 - less memory (2300k max), slower) //
-//		0 // verbosity (0, don't talk) //
-//		);
-//	// TODO: Assert on uncompress error //
-//	//if ( Error != BZ_OK )
-//	
-//	// Return the uncompressed buffer //
-//	return Uncompressed;
-//}
+// - ------------------------------------------------------------------------------------------ - //
+// Decode packed BZIP data to a new Array //
+template< class Type >
+inline Array<Type>* unpack_BZIP_Array( char* _Src, const size_t _SrcSize ) {
+
+}
 // - ------------------------------------------------------------------------------------------ - //
 // Encode packed BZIP data to a new Array //
 template< class Type >
@@ -80,39 +37,6 @@ inline DataBlock* pack_BZIP_Array( Array<Type>* _Src ) {
 	delete_DataBlock( Source );
 	
 	return Compressed;
-	
-//	// Allocate worst case space to store compressed data //
-//	Array<Type>* Compressed = new_Array<Type>( ((_Src->Size * sizeof(Type)) + ((_Src->Size * sizeof(Type)) / 100) + 600 + 4) / sizeof(Type) );
-//	
-//	size_t* UncompressedSize = reinterpret_cast<size_t*>(&Compressed->Data[0]);
-//	*UncompressedSize = 0;
-//	size_t CompressedSize = (Compressed->Size * sizeof(Type))-4;
-//	
-//	// Compress the Data //
-//	int Error = BZ2_bzBuffToBuffCompress(
-//		(char*)(&(Compressed->Data[4])),
-//		&CompressedSize,
-//		(char*)_Src->Data,
-//		_Src->Size,
-//		9, // blockSize100k (value * 100000 bytes allocated for work spcae (1-9)) //
-//		0, // verbosity (0, don't talk) //
-//		0 // workFactor (0 defaults to recommended case, 30) //
-//		);
-//	// TODO: Assert on compress error //
-//	//if ( Error != BZ_OK )
-//
-//	
-//	// Store Sizes //
-//	Compressed->Size = (CompressedSize + 4) / sizeof( Type );
-//	*UncompressedSize = _Src->Size;
-//
-//	// If bzip worked, bzip will have changed the size in the Array to less than the //
-//	//   memory allocated.  Thusly, we can call reallocate to force the data buffer to be //
-//	//   re-allocated to reduce memory usage (causing normal OS fragmentation). //
-//	reallocate_Array( &Compressed );
-//	
-//	// Return the compressed buffer //
-//	return Compressed;
 }
 //// - ------------------------------------------------------------------------------------------ - //
 //
--- Code/Library/Data/Array_LZMA.h
+++ Code/Library/Data/Array_LZMA.h
@@ -4,132 +4,96 @@
 #ifndef __Library_Data_Array_LZMA_H__
 #define __Library_Data_Array_LZMA_H__
 // - ------------------------------------------------------------------------------------------ - //
-#include "Array.h"
-#include <External/Lzma/LzmaDecode.h>
+#include "DataBlock_Core.h"
+#include "DataBlock_LZMA.h"
+
+#include "Array_Core.h"
 // - ------------------------------------------------------------------------------------------ - //
 //namespace Data {
 // - ------------------------------------------------------------------------------------------ - //
 // Decode packed LZMA data to a new Array //
 template< class Type >
-inline Array<Type>* unpack_LZMA_Array( const Array<Type>* _Src ) {
-	unsigned char LZMAProperties[LZMA_PROPERTIES_SIZE];
+inline Array<Type>* unpack_LZMA_Array( DataBlock* _Src ) {
+	DataBlock* Uncompressed = unpack_LZMA_DataBlock( _Src );
+	Array<Type>* UncompressedArray = new_Array<Type>( Uncompressed );
 	
-	// Copy first byte of LZMA Properties.  Somehow, this is vitally important. //
-	*LZMAProperties = _Src->Data[0];
+	delete_DataBlock( Uncompressed );
 	
-	// Get the Uncompressed Size from the properties //
-	unsigned long long int UncompressedSize = *((unsigned long long int*)&_Src->Data[LZMA_PROPERTIES_SIZE]);
-	
-	// Decoder State structure //
-	CLzmaDecoderState state;
-	
-	// Decode the properties //
-	if( LzmaDecodeProperties( &state.Properties, LZMAProperties, LZMA_PROPERTIES_SIZE) != LZMA_RESULT_OK ) {
-		//Log( LOG_MESH_INFO, "Incorrect stream properties (LZMA.h)" );
- 	  	return 0;
-    }
-	
-	CProb Probs[ LzmaGetNumProbs(&state.Properties) ];
-	state.Probs = (UInt16*)&Probs;
-	
-	// Allocate memory for "Probs" //
-//	state.Probs = new CProb[ LzmaGetNumProbs(&state.Properties) ];
-//	if (state.Probs == 0) {
-//		//Log( LOG_MESH_INFO, "state.Probs == 0 (LZMA.h)" );
-//		return 0;
-//	}
-	
-	// Allocate a new Array for our uncompressed Data //
-	Array<Type>* UBuffer = new_Array<Type>( UncompressedSize );
+	return UncompressedArray;	
 
-	// Temporary variables modified by decode function, so to learn about how it went //
-	size_t inProcessed = 0;
-	size_t outProcessed = 0;
-	
-	// Decode the file //
-	int Result = LzmaDecode( 
-		&state,
-		( unsigned char* )&_Src->Data[ LZMA_PROPERTIES_SIZE + 8 ],
-		_Src->Size,
-		&inProcessed,
-		( unsigned char* )UBuffer->Data,
-		UncompressedSize,
-		&outProcessed
-		);
-	// TODO: React to result of the decode //
-	
-	// Clean up work memory //
-//	delete [] state.Probs;
-	
-	// Return our new LZMA decompressed data //
-	return UBuffer;
-}
-// - ------------------------------------------------------------------------------------------ - //
-// Decode packed LZMA data to a passed Array //
-template< class Type >
-inline const size_t unpack_LZMA_Array( const Array<Type>* _Src, Array<Type>* _Dest ) {
-	unsigned char LZMAProperties[LZMA_PROPERTIES_SIZE];
-	
-	// Copy first byte of LZMA Properties.  Somehow, this is vitally important. //
-	*LZMAProperties = _Src->Data[0];
-	
-	// Get the Uncompressed Size from the properties //
-	unsigned long long int UncompressedSize = *((unsigned long long int*)&_Src->Data[LZMA_PROPERTIES_SIZE]);
-	
-	// If the passed Array is too small for our uncompressed data, fail //
-	if ( UncompressedSize > _Dest->Size ) {
-		// Not enough memory available in passed block! //
-		return 0;
-	}
-	
-	// Decoder State structure //
-	CLzmaDecoderState state;
-	
-	// Decode the properties //
-	if( LzmaDecodeProperties( &state.Properties, LZMAProperties, LZMA_PROPERTIES_SIZE) != LZMA_RESULT_OK ) {
-		//Log( LOG_MESH_INFO, "Incorrect stream properties (LZMA.h)" );
- 	  	return 0;
-    }
-
-	CProb Probs[ LzmaGetNumProbs(&state.Properties) ];
-	state.Probs = (UInt16*)&Probs;
-
+//	unsigned char LZMAProperties[LZMA_PROPERTIES_SIZE];
+//	
+//	// Copy first byte of LZMA Properties.  Somehow, this is vitally important. //
+//	*LZMAProperties = _Src->Data[0];
+//	
+//	// Get the Uncompressed Size from the properties //
+//	unsigned long long int UncompressedSize = *((unsigned long long int*)&_Src->Data[LZMA_PROPERTIES_SIZE]);
+//	
+//	// Decoder State structure //
+//	CLzmaDecoderState state;
+//	
+//	// Decode the properties //
+//	if( LzmaDecodeProperties( &state.Properties, LZMAProperties, LZMA_PROPERTIES_SIZE) != LZMA_RESULT_OK ) {
+//		//Log( LOG_MESH_INFO, "Incorrect stream properties (LZMA.h)" );
+// 	  	return 0;
+//    }
+//	
+//	CProb Probs[ LzmaGetNumProbs(&state.Properties) ];
+//	state.Probs = (UInt16*)&Probs;
+//	
 //	// Allocate memory for "Probs" //
-//	state.Probs = new CProb[ LzmaGetNumProbs(&state.Properties) ];
-//	if (state.Probs == 0) {
-//		//Log( LOG_MESH_INFO, "state.Probs == 0 (LZMA.h)" );
-//		return 0;
-//	}
-	
-	// Temporary variables modified by decode function, so to learn about how it went //
-	size_t inProcessed = 0;
-	size_t outProcessed = 0;
-	
-	// Decode the file //
-	int Result = LzmaDecode( 
-		&state,
-		( unsigned char* )&_Src->Data[ LZMA_PROPERTIES_SIZE + 8 ],
-		_Src->Size,
-		&inProcessed,
-		( unsigned char* )_Dest->Data,
-		UncompressedSize,
-		&outProcessed
-		);
-	// TODO: React to result of the decode //
-	
-	// Clean up work memory //
-//	delete [] state.Probs;
-	
-	// Return the size of the uncompressed data on success //
-	return UncompressedSize;
+////	state.Probs = new CProb[ LzmaGetNumProbs(&state.Properties) ];
+////	if (state.Probs == 0) {
+////		//Log( LOG_MESH_INFO, "state.Probs == 0 (LZMA.h)" );
+////		return 0;
+////	}
+//	
+//	// Allocate a new Array for our uncompressed Data //
+//	Array<Type>* UBuffer = new_Array<Type>( UncompressedSize );
+//
+//	// Temporary variables modified by decode function, so to learn about how it went //
+//	size_t inProcessed = 0;
+//	size_t outProcessed = 0;
+//	
+//	// Decode the file //
+//	int Result = LzmaDecode( 
+//		&state,
+//		( unsigned char* )&_Src->Data[ LZMA_PROPERTIES_SIZE + 8 ],
+//		_Src->Size,
+//		&inProcessed,
+//		( unsigned char* )UBuffer->Data,
+//		UncompressedSize,
+//		&outProcessed
+//		);
+//	// TODO: React to result of the decode //
+//	
+//	// Clean up work memory //
+////	delete [] state.Probs;
+//	
+//	// Return our new LZMA decompressed data //
+//	return UBuffer;
 }
+// - ------------------------------------------------------------------------------------------ - //
+//// Decode packed LZMA data to a passed Array //
+//template< class Type >
+//inline const size_t unpack_LZMA_Array( DataBlock* _Src, Array<Type>* _Dest ) {
+//	DataBlock* Uncompressed = unpack_LZMA_DataBlock( _Src );
+//	Array<Type>* UncompressedArray = new_Array<Type>( Uncompressed );
+//	
+//	delete_DataBlock( Uncompressed );
+//	
+//	return UncompressedArray;	
+//	
+////	// Return the size of the uncompressed data on success //
+////	return UncompressedSize;
+//}
 // - ------------------------------------------------------------------------------------------ - //
 
 // - ------------------------------------------------------------------------------------------ - //
-template< class Type >
-const cArray<Type> cArray<Type>::UnpackLZMA( const cArray<Type>& _Src ) {
-	return cArray<Type>( unpack_LZMA_Array<Type>( _Src._Data ) );
-}
+//template< class Type >
+//const cArray<Type> cArray<Type>::UnpackLZMA( const cArray<Type>& _Src ) {
+//	return cArray<Type>( unpack_LZMA_Array<Type>( _Src._Data ) );
+//}
 // - ------------------------------------------------------------------------------------------ - //
 
 // - ------------------------------------------------------------------------------------------ - //
