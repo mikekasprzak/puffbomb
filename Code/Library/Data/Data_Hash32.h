76bf66d37978ead33fba050713cf2f1641cf4ad8
mike <mike@d14347b8-48fd-0310-9e84-97907d434d85>
2008-01-23 21:03:57 +0000

    Added proper headers for String.  Moved the hash out of Data directly in to a Hash32 header for the string.
    git-svn-id: svn://10.0.50.55/puffbomb/trunk@2943 d14347b8-48fd-0310-9e84-97907d434d85

--- Code/Library/Data/Data_Hash32.h
+++ Code/Library/Data/Data_Hash32.h
@@ -11,19 +11,6 @@
 // circular shift hash -- produces good results if modding by a prime;
 // longword at a time would be faster (need alpha-style "is any byte 0"),
 // or just use the first longword
-// - ------------------------------------------------------------------------------------------ - // 
-inline unsigned int hash32_String( const char *str ) {
-	if (!str)
-		return 0;
- 
-	unsigned char *n = (unsigned char *) str;
-	unsigned int acc = 0x55555555;
-	
-	while (*n)
-		acc = (acc >> 27) + (acc << 5) + *n++;
-
-	return acc;
-}
 // - ------------------------------------------------------------------------------------------ - //
 inline unsigned int hash32_Data( const char *str, size_t size ) {
 	if (!str)
