2e1401fd9b96dfd1d12d2ffb0b43d25c93cc020a
mike <mike@d14347b8-48fd-0310-9e84-97907d434d85>
2008-02-08 22:25:15 +0000

    Added Directory.  As soon as this works, I should add "Archive", which is the same thing, but with actual data instead of just strings.
    git-svn-id: svn://10.0.50.55/puffbomb/trunk@2944 d14347b8-48fd-0310-9e84-97907d434d85

--- Code/Library/Data/Directory.h
+++ Code/Library/Data/Directory.h
@@ -7,42 +7,42 @@
 #include <cstring>
 #include <cstdio>
 
-#include "DataBlock.h"
+#include "Array.h"
+#include "Heap.h"
+// - ------------------------------------------------------------------------------------------ - //
+// TODO: When indexing a string, consider attaching the name (BaseName) to the front.
 // - ------------------------------------------------------------------------------------------ - //
 //namespace Data {
 // - ------------------------------------------------------------------------------------------ - //
 struct Directory {
+	// The given Base Directory name (include the trailing backslash .. ?) //
 	char* Name;
-	size_t Size;
 	
-	int* Index;
-	DataBlock* String;
+	Heap* String;
 };
 // - ------------------------------------------------------------------------------------------ - //
 
 
 // - ------------------------------------------------------------------------------------------ - //
-inline Directory* new_Directory( const char* _BaseName ) {
+inline Directory* new_Directory( const char* _BaseName = "" ) {
 	Directory* NewDir = new Directory;
 	
 	size_t NameLength = strlen(_BaseName);
 	
-	NewDir.Name = new char[NameLength];
-	memcpy( NewDir.Name, _BaseName, NameLength );
-	Size = 0;
+	NewDir->Name = new char[NameLength];
+	memcpy( NewDir->Name, _BaseName, NameLength );
 	
+	// Allocate some starting room.  Should be large enough for "." and "..". //
+	NewDir->String = new_Heap(2, 16);
 	
+	// Work //
 	
-	
-	//DataBlock* p = (DataBlock*)(new char[_Size+4]);
-	DataBlock* p = reinterpret_cast<DataBlock*>(new char[_Size+4]);
-	p->Size = _Size;
 	return NewDir;
 }
 // - ------------------------------------------------------------------------------------------ - //
-inline void delete_DataBlock( DataBlock* p ) {
-	//delete [] (char*)p;
-	delete [] reinterpret_cast<char*>(p);
+inline void delete_Directory( Directory* p ) {
+	delete [] p->Name;
+	delete_Heap( p->String );
 }
 // - ------------------------------------------------------------------------------------------ - //
 
