30a401298369a8d78fc2ba95eb8815bf51210f2a
mike <mike@d14347b8-48fd-0310-9e84-97907d434d85>
2008-02-11 00:18:42 +0000

    Added a whole bunch of string function.
    git-svn-id: svn://10.0.50.55/puffbomb/trunk@2950 d14347b8-48fd-0310-9e84-97907d434d85

--- Code/Library/Data/String_Core.h
+++ Code/Library/Data/String_Core.h
@@ -4,10 +4,68 @@
 #ifndef __Library_Data_String_Core_H__
 #define __Library_Data_String_Core_H__
 // - ------------------------------------------------------------------------------------------ - //
+// String functions do not automatically include the null character in length calculations,
+//   unless they are building a string. //
+// - ------------------------------------------------------------------------------------------ - //
 //namespace Data {
 // - ------------------------------------------------------------------------------------------ - //
 
+// - ------------------------------------------------------------------------------------------ - //
+inline const size_t length_String( const char* Str ) {
+	return strlen( Str );	
+}
+// - ------------------------------------------------------------------------------------------ - //
+// Note: strcpy needs enough room in Destination to hold the source string and a null character //
+inline const char* copy_String( const char* Src, char* Dest ) {
+	return strcpy( Dest, Src );
+}
+// - ------------------------------------------------------------------------------------------ - //
+// Note: strcat needs enough room in Destination to hold both strings and a null character //
+inline const char* cat_String( const char* Src, char* Dest ) {
+	return strcat( Dest, Src );
+}
+// - ------------------------------------------------------------------------------------------ - //
 
+
+// - ------------------------------------------------------------------------------------------ - //
+inline char* copy_String( const char* Src ) {
+	char* Copy = new char[ length_String( Src ) + 1 ];
+	
+	copy_String( Src, Copy );
+	
+	return Copy;
+}
+// - ------------------------------------------------------------------------------------------ - //
+inline char* new_String( const char* Src ) {
+	return copy_String( Src );
+}
+// - ------------------------------------------------------------------------------------------ - //
+inline void delete_String( char* Src ) {
+	delete [] Src;	
+}
+// - ------------------------------------------------------------------------------------------ - //
+
+// - ------------------------------------------------------------------------------------------ - //
+// Create a new string by concatonating two strings together. //
+inline char* new_String( const char* StrA, const char* StrB ) {
+	size_t StrALen = length_String( StrA );
+	size_t StrBLen = length_String( StrB );
+	
+	char* Str = new char[ StrALen + StrBLen + 1 ];
+	copy_String( StrA, Str );
+	cat_String( StrB, Str );
+	
+	return Str;
+}
+// - ------------------------------------------------------------------------------------------ - //
+
+
+// - ------------------------------------------------------------------------------------------ - //
+// Returns a pointer to where in the string the pattern is found, otherwise a null pointer. //
+inline const char* find_String( const char* Pattern, const char* Str ) {
+	return strstr( Str, Pattern );
+}
+// - ------------------------------------------------------------------------------------------ - //
 
 // - ------------------------------------------------------------------------------------------ - //
 //}; // namepsace Data //
