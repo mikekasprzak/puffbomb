7a3458f7a9a3d22fd51c9e84132c83eef1b3df0f
mike <mike@d14347b8-48fd-0310-9e84-97907d434d85>
2008-02-12 03:37:27 +0000

    Upgraded Array to the file code.
    git-svn-id: svn://10.0.50.55/puffbomb/trunk@2968 d14347b8-48fd-0310-9e84-97907d434d85

--- Code/Library/Data/Array_File.h
+++ Code/Library/Data/Array_File.h
@@ -14,7 +14,8 @@
 template< class Type >
 inline Array<Type>* new_Array( const char* _FileName ) {
 	// Open File //
-	FILE* fp = fopen( _FileName, "rb" );
+//	FILE* fp = fopen( _FileName, "rb" );
+	FILE* fp = open_readonly_File( _FileName );
 	if ( fp == 0 ) {
 		// Return a dummy instance.  This might be bad, since an empty file will look the same. //
 		//return new_Array<Type>( (size_t)0 );
@@ -24,19 +25,23 @@ inline Array<Type>* new_Array( const char* _FileName ) {
 	}
 	
 	// Determine how large file is //
-	fseek( fp, 0, SEEK_END );
-	size_t Size = ftell( fp );
-	rewind( fp );
+//	fseek( fp, 0, SEEK_END );
+//	size_t Size = ftell( fp );
+//	rewind( fp );
+	size_t Size = size_File( fp );
 	
 	// Allocate space (Size is automatically set inside new_DataBlock) //
 	Array<Type>* p = new_Array<Type>( Size / sizeof(Type) );
+	
 	// TODO: Assert failure allocating block //
 	
 	// Read data //
-	fread( p->Data, 1, Size, fp );
+	//fread( p->Data, 1, Size, fp );
+	read_File( fp, (char*)&p->Data[0], Size );
 	
 	// Close file //
-	fclose( fp );
+	//fclose( fp );
+	close_File( fp );
 	
 	// Return data //
 	return p;
@@ -52,24 +57,28 @@ inline Array<Type>* new_Array( const char* _FileName ) {
 template< class Type >
 inline const size_t read_Array( Array<Type>* p, const char* _FileName ) {
 	// Open File //
-	FILE* fp = fopen( _FileName, "rb" );
+//	FILE* fp = fopen( _FileName, "rb" );
+	FILE* fp = open_readonly_File( _FileName );
 	if ( fp == 0 ) {
 		// TODO: Log file open error //
 		return 0;
 	}
 	
 	// Determine how large file is //
-	fseek( fp, 0, SEEK_END );
-	size_t Size = ftell( fp );
-	rewind( fp );
+//	fseek( fp, 0, SEEK_END );
+//	size_t Size = ftell( fp );
+//	rewind( fp );
+	size_t Size = size_File( fp );
 	
 	size_t DataSize = p->Size * sizeof(Type);
 	
 	// Read data (only as much as the smallest size) //
-	size_t BytesRead = fread( p->Data, 1, Size > DataSize ? DataSize : Size, fp );
+//	size_t BytesRead = fread( p->Data, 1, Size > DataSize ? DataSize : Size, fp );
+	size_t BytesRead = read_File( fp, p->Data, Size > DataSize ? DataSize : Size );
 	
 	// Close file //
-	fclose( fp );
+//	fclose( fp );
+	close_File( fp );
 	
 	// Return the number of bytes read //
 	return BytesRead;
@@ -78,18 +87,21 @@ inline const size_t read_Array( Array<Type>* p, const char* _FileName ) {
 template< class Type >
 inline const size_t write_Array( Array<Type>* p, const char* _FileName ) {
 	// Open File //
-	FILE* fp = fopen( _FileName, "wb" );
+//	FILE* fp = fopen( _FileName, "wb" );
+	FILE* fp = open_writeonly_File( _FileName );
 	if ( fp == 0 ) {
-		// TODO: Log file open error //
 		return 0;
 	}
 	
 	// Write the data //
-	size_t BytesWritten = fwrite( p->Data, 1, p->Size * sizeof(Type), fp );
+//	size_t BytesWritten = fwrite( p->Data, 1, p->Size * sizeof(Type), fp );
+	size_t BytesWritten = write_File( fp, p->Data, p->Size * sizeof(Type) );
+
 	// TODO: Assert on fire write error //
 	
 	// Close file //
-	fclose( fp );
+//	fclose( fp );
+	close_File( fp );
 		
 	// Return the number of bytes read //
 	return BytesWritten;
@@ -116,7 +128,8 @@ inline const size_t read_Array( Array<Type>* p, FILE* fp ) {
 	size_t DataSize = p->Size * sizeof(Type);
 	
 	// Read data (only as much as the smallest size) //
-	size_t BytesRead = fread( p->Data, 1, Size > DataSize ? DataSize : Size, fp );
+//	size_t BytesRead = fread( p->Data, 1, Size > DataSize ? DataSize : Size, fp );
+	size_t BytesRead = read_File( fp, p->Data, Size > DataSize ? DataSize : Size );
 	
 	// Return the number of bytes read //
 	return BytesRead;
@@ -128,11 +141,11 @@ inline const size_t write_Array( Array<Type>* p, FILE* fp ) {
 	write_File( fp, p->Size );
 
 	// Write the data //
-	size_t BytesWritten = fwrite( p->Data, 1, p->Size * sizeof(Type), fp );
+	//size_t BytesWritten = fwrite( p->Data, 1, p->Size * sizeof(Type), fp );
+	size_t BytesWritten = write_File( fp, p->Data, p->Size * sizeof(Type) );
 
 	// TODO: Assert on fire write error //
-	
-		
+			
 	// Return the number of bytes read //
 	return BytesWritten;
 }
