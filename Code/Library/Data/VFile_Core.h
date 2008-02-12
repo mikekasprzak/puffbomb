a044986ed9355e63ce2aedbed289c5599a5de75f
mike <mike@d14347b8-48fd-0310-9e84-97907d434d85>
2008-02-12 14:42:58 +0000

    Added VFILE.
    git-svn-id: svn://10.0.50.55/puffbomb/trunk@2981 d14347b8-48fd-0310-9e84-97907d434d85

---
+++ Code/Library/Data/VFile_Core.h
@@ -0,0 +1,144 @@
+// - ------------------------------------------------------------------------------------------ - //
+// VFile - "FILE*" like implementation that's to a virtual file in RAM //
+// - ------------------------------------------------------------------------------------------ - //
+#ifndef __Library_Data_VFile_Core_H__
+#define __Library_Data_VFile_Core_H__
+// - ------------------------------------------------------------------------------------------ - //
+// TODO: Add a function for reading a DataBlock from an open file //
+// TODO: Also add functions for reading data of an expected size, zero terminated strings, etc. //
+// TODO: Add functions for reading/adapting floating point numbers from IEEE to other needed types
+// TODO: Add functions for converting/writing floats to fixed point numbers (a cheat)
+// - ------------------------------------------------------------------------------------------ - //
+#include "Array.h"
+
+#include "Endian.h"
+// - ------------------------------------------------------------------------------------------ - //
+//namespace Data {
+// - ------------------------------------------------------------------------------------------ - //
+struct VFILE {
+	Array<char>* Data;
+};
+// - ------------------------------------------------------------------------------------------ - //
+
+
+// - ------------------------------------------------------------------------------------------ - //
+// Get the size of an open file, in bytes //
+// - ------------------------------------------------------------------------------------------ - //
+inline const size_t size_File( VFILE* fp ) {
+	return fp->Data->Size;
+}
+// - ------------------------------------------------------------------------------------------ - //
+
+// - ------------------------------------------------------------------------------------------ - //
+// Opening files //
+// - ------------------------------------------------------------------------------------------ - //
+inline VFILE* open_VFile( char* = "rb" ) {
+	VFILE* MyFile = new VFILE;
+	MyFile->Data = new Array<char>();
+	return MyFile;
+}
+// - ------------------------------------------------------------------------------------------ - //
+inline VFILE* open_readonly_VFile( ) {
+	return open_File();
+}
+// - ------------------------------------------------------------------------------------------ - //
+inline VFILE* open_writeonly_VFile( ) {
+	return open_File();
+}
+// - ------------------------------------------------------------------------------------------ - //
+
+// NOTE: C library has 2 interesting functions.  tmpfile() and tmpname( .. ).  tmpfile creates //
+//   a temorary file instance, that erases itself when closed.  tmpname returns a legit temporary //
+//   filename, that does not conflict (with what, I don't know).  If you choose to open the file, //
+//   don't forget to delete it with "remove( "File.blah" );". //
+
+
+// - ------------------------------------------------------------------------------------------ - //
+// Closing Files //
+// - ------------------------------------------------------------------------------------------ - //
+inline void close_VFile( VFILE* fp ) {
+	delete_Array<char>( fp->Data );
+	
+	delete fp;
+}
+// - ------------------------------------------------------------------------------------------ - //
+
+
+// NOTE: fread( TargetPointer, DataSize, Count, FilePointer ); //
+
+//// - ------------------------------------------------------------------------------------------ - //
+//// Templated read functions (read_File<int>( fp );) //
+//// - ------------------------------------------------------------------------------------------ - //
+//template< class Type >
+//inline const Type read_VFile( VFILE* fp ) {
+//	Type Target;
+//	fread( &Target, sizeof(Target), 1, fp );
+//	return Target;
+//}
+//// - ------------------------------------------------------------------------------------------ - //
+//template< class Type >
+//inline const Type readswap_VFile( VFILE* fp ) {
+//	Type Target;
+//	fread( &Target, sizeof(Target), 1, fp );
+//	return byteswap(Target);
+//}
+//// - ------------------------------------------------------------------------------------------ - //
+//template< class Type >
+//inline const Type readbe_VFile( VFILE* fp ) {
+//	Type Target;
+//	fread( &Target, sizeof(Target), 1, fp );
+//	return beswap(Target);
+//}
+//// - ------------------------------------------------------------------------------------------ - //
+//template< class Type >
+//inline const Type readle_VFile( VFILE* fp ) {
+//	Type Target;
+//	fread( &Target, sizeof(Target), 1, fp );
+//	return leswap(Target);
+//}
+//// - ------------------------------------------------------------------------------------------ - //
+//
+//// - ------------------------------------------------------------------------------------------ - //
+//inline const size_t read_VFile( VFILE* fp, char* Data, const size_t Size ) {
+//	return fread( Data, Size, 1, fp );
+//}
+//// - ------------------------------------------------------------------------------------------ - //
+//
+//// - ------------------------------------------------------------------------------------------ - //
+//// Templated read functions (read_File<int>( fp );) //
+//// - ------------------------------------------------------------------------------------------ - //
+//template< class Type >
+//inline const size_t write_VFile( VFILE* fp, const Type Data ) {
+//	return fwrite( &Data, sizeof(Data), 1, fp );
+//}
+//// - ------------------------------------------------------------------------------------------ - //
+//template< class Type >
+//inline const size_t writeswap_VFile( VFILE* fp, const Type Data ) {
+//	Type Copy = byteswap(Data);
+//	return fwrite( &Copy, sizeof(Data), 1, fp );
+//}
+//// - ------------------------------------------------------------------------------------------ - //
+//template< class Type >
+//inline const size_t writebe_VFile( VFILE* fp, const Type Data ) {
+//	Type Copy = beswap(Data);
+//	return fwrite( &Copy, sizeof(Data), 1, fp );
+//}
+//// - ------------------------------------------------------------------------------------------ - //
+//template< class Type >
+//inline const size_t writele_VFile( VFILE* fp, const Type Data ) {
+//	Type Copy = leswap(Data);
+//	return fwrite( &Copy, sizeof(Data), 1, fp );
+//}
+//// - ------------------------------------------------------------------------------------------ - //
+//
+//// - ------------------------------------------------------------------------------------------ - //
+//inline const size_t write_VFile( VFILE* fp, const char* Data, const size_t Size ) {
+//	return fwrite( Data, Size, 1, fp );
+//}
+//// - ------------------------------------------------------------------------------------------ - //
+
+// - ------------------------------------------------------------------------------------------ - //
+//}; // namespace Data //
+// - ------------------------------------------------------------------------------------------ - //
+#endif // __Library_Data_VFile_Core_H__ //
+// - ------------------------------------------------------------------------------------------ - //
