58431f9f03c340a7759eeb0557d26bc08039435e
mike <mike@d14347b8-48fd-0310-9e84-97907d434d85>
2008-02-12 18:25:07 +0000

    Cleaned up Directory_UnixDir.h.  Comments and todo's at the top were wrong.
    git-svn-id: svn://10.0.50.55/puffbomb/trunk@2991 d14347b8-48fd-0310-9e84-97907d434d85

--- Code/Library/Data/Directory_UnixDir.h
+++ Code/Library/Data/Directory_UnixDir.h
@@ -1,5 +1,5 @@
 // - ------------------------------------------------------------------------------------------ - //
-// Directory - File Name tree cataloging library //
+// Directory UnixDir - Unix Style implementation of polling a directory tree //
 // - ------------------------------------------------------------------------------------------ - //
 #ifndef __Library_Data_Directory_UnixDir_H__
 #define __Library_Data_Directory_UnixDir_H__
@@ -8,13 +8,6 @@
 #include <sys/stat.h>
 
 #include "Directory_Core.h"
-// - ------------------------------------------------------------------------------------------ - //
-// Directories are lists of file names.  They can be populated manually, or be polled from disk
-//   and other sources.  The next step up from a Directory is an Archive, which contains data too.
-//
-// TODO: When indexing a string, consider attaching the name (BaseName/) to the front.
-// TODO: Searching for a file (given a pattern to match)
-// TODO: Saving and loading Directories to and from disk/streams. (Need FILE* library)
 // - ------------------------------------------------------------------------------------------ - //
 //namespace Data {
 // - ------------------------------------------------------------------------------------------ - //
